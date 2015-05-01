9008a46a9db0924f9719b1dc91f36d6cf547bc98
yoichi kobayashi <info@tplh.net>
2015-05-01 20:52:54 +0900

    TrackballControlesを追加。各種lightを追加。

--- src/js/unique.js
+++ src/js/unique.js
@@ -1,20 +1,31 @@
-window.addEventListener('load', function() {
-  threeStart();
-}, false);
+var lastTimeRender;
+var renderer;
+var scene;
+var canvas;
+var camera;
+var trackball;
+var directionalLight;
+var pointLight;
+var spotLight;
+var hemisphereLight;
+var ambientLight;
+var directionalLightHelper;
+var pointLightHelper;
+var spotLightHelper;
+var hemisphereLightHelper;
+var axis;
+var box;
 
 var threeStart = function() {
   initThree();
   initCamera();
   initObject();
-  draw();
+  initLight();
+  renderloop();
 };
 
-var renderer;
-var scene;
-var canvasFrame;
-
 var initThree = function() {
-  canvasFrame = document.getElementById('canvas');
+  canvas = document.getElementById('canvas');
   renderer = new THREE.WebGLRenderer({
     antialias: true
   });
@@ -22,33 +33,85 @@ var initThree = function() {
     alert('Three.jsの初期化に失敗しました。');
   }
   renderer.setSize(bodyWidth, bodyHeight);
-  canvasFrame.appendChild(renderer.domElement);
+  canvas.appendChild(renderer.domElement);
   renderer.setClearColor(0x000000, 1.0);
   scene = new THREE.Scene();
 };
 
-var camera;
-
 var initCamera = function() {
   camera = new THREE.PerspectiveCamera(45, bodyWidth / bodyHeight, 1, 10000);
-  camera.position.set(50, 50, 100);
+  camera.position.set(100, 100, 100);
   camera.up.set(0, 0, 1);
   camera.lookAt({
     x: 0,
     y: 0,
     z: 0
-  })
+  });
+  
+  trackball = new THREE.TrackballControls(camera, canvas);
+  trackball.screen.width = bodyWidth;
+  trackball.screen.height = bodyHeight;
+  trackball.noRotate = false;
+  trackball.rotateSpeed = 4;
+  trackball.noZoom = false;
+  trackball.zoomSpeed = 1;
+  trackball.noPan = true;
 };
 
-var axis;
+var initLight = function() {
+  directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  pointLight = new THREE.PointLight(0xffffff, 1);
+  spotLight = new THREE.SpotLight(0xffffff, 1, 200, getRadian(10));
+  hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+  ambientLight = new THREE.AmbientLight(0xffffff);
+
+  directionalLight.position.set(60, 60, 90);
+  pointLight.position.set(20, 20, 30);
+  spotLight.position.set(40, 40, 100);
+  hemisphereLight.position.set(50, 20, 70);
+  
+  directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
+  pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+  spotLightHelper = new THREE.SpotLightHelper(spotLight, 1);
+  hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 1);
+  
+  scene.add(spotLight);
+  scene.add(spotLightHelper);
+};
 
 var initObject = function() {
+  var geometry;
+  var material;
+  
   axis = new THREE.AxisHelper(50);
-  scene.add(axis);
   axis.position.set(0, 0, 0);
+  
+  geometry = new THREE.BoxGeometry(20, 20, 20);
+  material = new THREE.MeshLambertMaterial({
+    color: 0xff0000
+  });
+  box = new THREE.Mesh(geometry, material);
+  axis.position.set(0, 0, 0);
+
+  scene.add(axis);
+  scene.add(box);
 };
 
-var draw = function() {
+var render = function() {
   renderer.clear();
   renderer.render(scene, camera);
+  trackball.update();
 };
+
+var renderloop = function() {
+  var now = +new Date();
+  requestAnimationFrame(renderloop);
+
+  if (now - lastTimeRender < frameTime) {
+    return;
+  }
+  render();
+  lastTimeRender = +new Date();
+};
+
+threeStart();
