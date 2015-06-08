42063130d2f4c4b3aab46762daf370b200464879
yoichi kobayashi <info@tplh.net>
2015-06-08 23:57:27 +0900

    [add] watchifyを追加。

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Three.jsを使ったdemo用のテンプレート。
 ## npm install
 
 ```
-npm i -D browser-sync browserify gulp gulp-ruby-sass gulp-sequence require-dir vinyl-source-stream
+npm i -D browser-sync browserify gulp gulp-ruby-sass gulp-sequence require-dir vinyl-source-stream watchify
 ```
--- tasks/browserify.js
+++ tasks/browserify.js
@@ -1,13 +1,49 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var CONFIG = require('../package.json').projectConfig;
 
+var browserifyOpts = {
+  entries: ['./' + CONFIG.SRC + '/js/main.js']
+};
+
+var bundler = function(watch) {
+  var b;
+
+  if (watch) {
+    browserifyOpts.debug = true;
+    browserifyOpts.cache = {};
+    browserifyOpts.packageCache = {};
+    browserifyOpts.fullPath = true;
+    b = watchify(browserify(browserifyOpts));
+  } else {
+    b = browserify(browserifyOpts);
+  }
+
+  var bundle = function() {
+    return b
+            .bundle()
+            .on('error', function(err) {
+              console.log('bundle error: ' + err);
+            })
+            .pipe(source('main.js'))
+            .pipe(gulp.dest('./js/'));
+  };
+
+  b
+    .on('update', bundle)
+    .on('log', function(message) {
+      return console.log(message);
+    });
+
+  return bundle();
+};
+
 gulp.task('browserify', function() {
-  return browserify({
-    entries: ['./' + CONFIG.SRC + '/js/main.js']
-  })
-    .bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest(CONFIG.DST + CONFIG.PATH + '/js'));
+  return bundler();
+});
+
+gulp.task('watchify', function() {
+  return bundler(true);
 });
