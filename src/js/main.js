9885058f7c20ed2e3487d16fb7cc8b1369a73c03
yoichi kobayashi <info@tplh.net>
2015-06-26 14:57:34 +0900

    [add] 独自Geometry追加。verticesの操作実験。

--- src/js/main.js
+++ src/js/main.js
@@ -39,20 +39,57 @@ var initThree = function() {
 
 var init = function() {
   var ballGeometry = new THREE.SphereGeometry(240, 24, 24);
-  var ballMaterial = new THREE.MeshPhongMaterial({
-    wireframe: true
+  var ballMaterial = new THREE.MeshLambertMaterial({
+    color: 0xffffff,
+    shading: THREE.FlatShading,
+    side: THREE.DoubleSide
   });
+  var baseGeometry = new THREE.Geometry();
   
+  var p = [];
+  var r = 100;
+  
+  p[0] = new THREE.Vector3(r * -1, r, r * -1);
+  p[1] = new THREE.Vector3(r * -1, r, r);
+  p[2] = new THREE.Vector3(r, r, r);
+  p[3] = new THREE.Vector3(r, r, r * -1);
+  p[4] = new THREE.Vector3(r * -1, r * -1, r * -1);
+  p[5] = new THREE.Vector3(r * -1, r * -1, r);
+  p[6] = new THREE.Vector3(r, r * -1, r);
+  p[7] = new THREE.Vector3(r, r * -1, r * -1);
+
+  for (var i = 0; i < p.length; i++) {
+    baseGeometry.vertices.push(p[i]);
+  };
+  baseGeometry.faces.push( new THREE.Face3(0, 1, 3));
+  baseGeometry.faces.push( new THREE.Face3(2, 3, 1));
+  baseGeometry.faces.push( new THREE.Face3(0, 4, 1));
+  baseGeometry.faces.push( new THREE.Face3(5, 1, 4));
+  baseGeometry.faces.push( new THREE.Face3(0, 3, 4));
+  baseGeometry.faces.push( new THREE.Face3(7, 4, 3));
+  baseGeometry.faces.push( new THREE.Face3(3, 2, 7));
+  baseGeometry.faces.push( new THREE.Face3(6, 7, 2));
+  baseGeometry.faces.push( new THREE.Face3(2, 1, 6));
+  baseGeometry.faces.push( new THREE.Face3(5, 6, 1));
+  baseGeometry.faces.push( new THREE.Face3(4, 7, 5));
+  baseGeometry.faces.push( new THREE.Face3(6, 5, 7));
+  baseGeometry.computeFaceNormals();
+  baseGeometry.computeVertexNormals();
+
   initThree();
   
   camera = new Camera();
   camera.init(bodyWidth, bodyHeight);
   
   light = new HemiLight();
-  light.init(scene, get.radian(90), 0, 1000, 0xeeeeee, 0x333333, 1);
+  light.init(scene, get.radian(45), get.radian(45), 1000, 0xeeeeee, 0x999999, 1);
   
   ball = new Mesh();
-  ball.init(scene, ballGeometry, ballMaterial);
+  ball.init(scene, baseGeometry, ballMaterial);
+  
+  p[0].normalize().multiplyScalar(r * 3);
+  baseGeometry.computeFaceNormals();
+  baseGeometry.computeVertexNormals();
   
   renderloop();
   debounce(window, 'resize', function(event){
