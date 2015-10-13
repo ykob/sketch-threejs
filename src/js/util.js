0f84d61cc881f30564229528cbc2d180601f95b5
Yoichi Kobayashi <info@tplh.net>
2015-10-14 00:02:20 +0900

    [modify] util クラスを簡素化。

--- src/js/util.js
+++ src/js/util.js
@@ -1,26 +1,19 @@
-var exports = function(){
-  var Util = function() {};
-  
-  Util.prototype.getRandomInt = function(min, max){
+var exports = {
+  getRandomInt: function(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
-  };
-  
-  Util.prototype.getDegree = function(radian) {
+  },
+  getDegree: function(radian) {
     return radian / Math.PI * 180;
-  };
-  
-  Util.prototype.getRadian = function(degrees) {
+  },
+  getRadian: function(degrees) {
     return degrees * Math.PI / 180;
-  };
-  
-  Util.prototype.getPointSphere = function(rad1, rad2, r) {
+  },
+  getSpherical: function(rad1, rad2, r) {
     var x = Math.cos(rad1) * Math.cos(rad2) * r;
     var z = Math.cos(rad1) * Math.sin(rad2) * r;
     var y = Math.sin(rad1) * r;
     return [x, y, z];
-  };
-  
-  return Util;
+  }
 };
 
-module.exports = exports();
+module.exports = exports;
