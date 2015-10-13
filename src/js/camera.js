1cc9bdde2b8c3705db7c8d157512dc8d52eaddd9
Yoichi Kobayashi <info@tplh.net>
2015-10-13 23:59:23 +0900

    [WIP] camera と light のクラスを改修。途中まで。

--- src/js/camera.js
+++ src/js/camera.js
@@ -1,34 +1,39 @@
 var Util = require('./util');
-var util = new Util();
 
 var exports = function(){
   var Camera = function() {
     this.width = 0;
     this.height = 0;
-    this.rad1 = 0;
-    this.rad2 = 0;
-    this.r = 1600;
+    this.rad1 = Util.getRadian(60);
+    this.rad2 = Util.getRadian(30);
+    this.range = 1600;
     this.obj;
   };
   
-  Camera.prototype.init = function(rad1, rad2, width, height) {
-    this.width = width;
-    this.height = height;
-    this.rad1 = rad1;
-    this.rad2 = rad2;
-    this.obj = new THREE.PerspectiveCamera(35, this.width / this.height, 1, 10000);
-    this.setPosition(this.rad1, this.rad2, this.r);
-  };
-  
-  Camera.prototype.setPosition = function(rad1, rad2) {
-    var points = util.getPointSphere(rad1, rad2, this.r);
-    this.obj.position.set(points[0], points[1], points[2]);
-    this.obj.up.set(0, 1, 0);
-    this.obj.lookAt({
-      x: 0,
-      y: 0,
-      z: 0
-    });
+  Camera.prototype = {
+    init: function(width, height) {
+      this.width = width;
+      this.height = height;
+      this.obj = new THREE.PerspectiveCamera(35, this.width / this.height, 1, 10000);
+      this.obj.up.set(0, 1, 0);
+      this.setPosition(this.rad1, this.rad2, this.r);
+      this.lookAtCenter();
+    },
+    reset: function() {
+      this.setPosition(this.rad1, this.rad2, this.r);
+      this.lookAtCenter();
+    },
+    setPosition: function() {
+      var points = Util.getSpherical(this.rad1, this.rad2, this.range);
+      this.obj.position.set(points[0], points[1], points[2]);
+    },
+    lookAtCenter: function() {
+      this.obj.lookAt({
+        x: 0,
+        y: 0,
+        z: 0
+      });
+    }
   };
 
   return Camera;
--- src/js/hemiLight.js
+++ src/js/hemiLight.js
@@ -1,24 +1,24 @@
 var Util = require('./util');
-var util = new Util();
 
 var exports = function(){
   var HemiLight = function() {
     this.rad1 = 0;
     this.rad2 = 0;
-    this.r = 0;
+    this.range = 0;
     this.obj;
   };
   
-  HemiLight.prototype.init = function(scene, rad1, rad2, r, hex1, hex2, intensity) {
-    this.r = r;
-    this.obj = new THREE.HemisphereLight(hex1, hex2, intensity);
-    this.setPosition(rad1, rad2);
-    scene.add(this.obj);
-  };
-  
-  HemiLight.prototype.setPosition = function(rad1, rad2) {
-    var points = util.getPointSphere(rad1, rad2, this.r);
-    this.obj.position.set(points[0], points[1], points[2]);
+  HemiLight.prototype = {
+    init: function(scene, rad1, rad2, range, hex1, hex2, intensity) {
+      this.range = range;
+      this.obj = new THREE.HemisphereLight(hex1, hex2, intensity);
+      this.setPosition(rad1, rad2);
+      scene.add(this.obj);
+    },
+    setPosition: function() {
+      var points = Util.getSpherical(this.rad1, this.rad2, this.r);
+      this.obj.position.set(points[0], points[1], points[2]);
+    }
   };
   
   return HemiLight;
