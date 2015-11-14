f4f567f16037627a084e8d755fd1271caf33f468
yoichi kobayashi <info@tplh.net>
2015-11-15 01:00:22 +0900

    [add] 物理演算の変数と処理を追加。

--- src/js/modules/pointLight.js
+++ src/js/modules/pointLight.js
@@ -1,4 +1,5 @@
 var Util = require('../modules/util');
+var Force = require('../modules/force');
 
 var exports = function(){
   var PointLight = function() {
@@ -10,6 +11,10 @@ var exports = function(){
     this.distance = 2000;
     this.decay = 1;
     this.obj;
+    this.velocity = new THREE.Vector3();
+    this.acceleration = new THREE.Vector3();
+    this.anchor = new THREE.Vector3();
+    this.mass = 1;
   };
   
   PointLight.prototype = {
@@ -22,7 +27,29 @@ var exports = function(){
     setPositionSpherical: function() {
       var points = Util.getSpherical(this.rad1, this.rad2, this.range);
       this.obj.position.copy(points);
-    }
+    },
+    updatePosition: function() {
+      this.obj.position.copy(this.velocity);
+    },
+    updateVelocity: function() {
+      this.acceleration.divideScalar(this.mass);
+      this.velocity.add(this.acceleration);
+    },
+    applyForce: function(vector) {
+      this.acceleration.add(vector);
+    },
+    applyFriction: function() {
+      var friction = Force.friction(this.acceleration, 0.1);
+      this.applyForce(friction);
+    },
+    applyDragForce: function(value) {
+      var drag = Force.drag(this.acceleration, value);
+      this.applyForce(drag);
+    },
+    hook: function(rest_length, k) {
+      var force = Force.hook(this.velocity, this.anchor, rest_length, k);
+      this.applyForce(force);
+    },
   };
   
   return PointLight;
--- src/js/modules/points.js
+++ src/js/modules/points.js
@@ -1,4 +1,5 @@
 var Util = require('../modules/util');
+var Force = require('../modules/force');
 
 var exports = function(){
   var Points = function() {
@@ -8,6 +9,7 @@ var exports = function(){
     this.velocity = new THREE.Vector3();
     this.acceleration = new THREE.Vector3();
     this.anchor = new THREE.Vector3();
+    this.mass = 1;
   };
   
   Points.prototype = {
@@ -30,17 +32,33 @@ var exports = function(){
       this.obj = new THREE.Points(this.geometry, this.material);
       param.scene.add(this.obj);
     },
-    updateVelocity: function() {
-      this.velocity.add(this.acceleration);
-    },
-    updatePosition: function() {
-      this.position.copy(this.velocity);
-    },
     updatePoints: function() {
       this.obj.geometry.attributes.position.needsUpdate = true;
       this.obj.geometry.attributes.vertexOpacity.needsUpdate = true;
       this.obj.geometry.attributes.size.needsUpdate = true;
-    }
+    },
+    updateVelocity: function() {
+      this.acceleration.divideScalar(this.mass);
+      this.velocity.add(this.acceleration);
+    },
+    updatePosition: function() {
+      this.obj.position.copy(this.velocity);
+    },
+    applyForce: function(vector) {
+      this.acceleration.add(vector);
+    },
+    applyFriction: function() {
+      var friction = Force.friction(this.acceleration, 0.1);
+      this.applyForce(friction);
+    },
+    applyDragForce: function(value) {
+      var drag = Force.drag(this.acceleration, value);
+      this.applyForce(drag);
+    },
+    hook: function(rest_length, k) {
+      var force = Force.hook(this.velocity, this.anchor, rest_length, k);
+      this.applyForce(force);
+    },
   };
 
   return Points;
