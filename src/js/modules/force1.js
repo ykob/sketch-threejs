da16bf817bd3b0d9d4fb67591329b7e7cfaa9d14
Yoichi Kobayashi <info@tplh.net>
2015-12-12 21:42:00 +0900

    [add] 1次元ベクトルの加速度計算クラス。

--- src/js/modules/force1.js
+++ src/js/modules/force1.js
@@ -8,7 +8,14 @@ var exports = function(){
     this.anchor = 0;
     this.mass = 1;
   };
-  
+  var normalize = function(num) {
+    num /= Math.abs(num);
+    if(isFinite(num)) {
+      return num;
+    } else {
+      return 0;
+    }
+  };
   Force1.prototype.updatePosition = function() {
     this.position = this.velocity;
   };
@@ -23,21 +30,21 @@ var exports = function(){
     var force = this.acceleration;
     if (!normal) normal = 1;
     force *= -1;
-    force /= force;
+    force = normalize(force);
     force *= mu;
     this.applyForce(force);
   };
   Force1.prototype.applyDrag = function(value) {
     var force = this.acceleration;
     force *= -1;
-    force /= force;
+    force = normalize(force);
     force *= this.acceleration * value;
     this.applyForce(force);
   };
   Force1.prototype.applyHook = function(rest_length, k) {
     var force = this.velocity - this.anchor;
-    var distance = force - rest_length;
-    force /= force;
+    var distance = Math.abs(force) - rest_length;
+    force = normalize(force);
     force *= -1 * k * distance;
     this.applyForce(force);
   };
