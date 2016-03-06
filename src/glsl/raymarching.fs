6fcb511a72f6ce2ab92e6d47df4987fe03fe5194
yoichi kobayashi <info@tplh.net>
2016-03-06 22:19:47 +0900

    [modify] sample code about ray marching.

--- src/glsl/raymarching.fs
+++ src/glsl/raymarching.fs
@@ -1,54 +1,43 @@
 precision highp float;
+
 uniform float time;
+uniform vec2 mouse;
 uniform vec2 resolution;
-// 微小な値
-const float EPS = 0.001;
-// 距離関数
-float distSphere(vec3 p, float r) {
-  return length(p) - r;
+
+const float sphere_size = 1.0;
+
+float getDistance(vec3 p) {
+  return length(p) - sphere_size;
 }
-float distScene(vec3 p) {
-  return distSphere(p, 1.3);
-}
-// 法線の計算
-vec3 getNormal(vec3 p) {
-  return normalize(vec3(
-    distScene(p + vec3(  EPS, 0.0, 0.0)) - distScene(p + vec3( -EPS, 0.0, 0.0)),
-    distScene(p + vec3(0.0,   EPS, 0.0)) - distScene(p + vec3(0.0,  -EPS, 0.0)),
-    distScene(p + vec3(0.0, 0.0,   EPS)) - distScene(p + vec3(0.0, 0.0,  -EPS))
-  ));
-}
-void main(void) {
-  // gl_FragCoord.xy の正規化
+
+void main() {
+  // fragment position
   vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-  // カメラとレイの定義
-  vec3 cPos  = vec3(0.0, 1.6, -2.0);
-  vec3 cDir  = normalize(vec3(0.0, -0.9, 1.0));
-  vec3 cUp   = cross(cDir, vec3(1.0, 0.0 ,0.0));
+
+  // camera
+  vec3 cPos = vec3(0.0,  0.0,  2.0);
+  vec3 cDir = vec3(0.0,  0.0, -1.0);
+  vec3 cUp  = vec3(0.0,  1.0,  0.0);
   vec3 cSide = cross(cDir, cUp);
-  float targetDepth = 1.3;
+  float targetDepth = 1.0;
+
+  // ray
   vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir * targetDepth);
-  // 距離関数を用いたレイのマーチングループ
-  float dist;
-  float depth = 0.0;
-  vec3 dPos = cPos;
-  for (int i = 0; i < 32; i++) {
-    dist = distScene(dPos);
-    depth += dist;
-    dPos = cPos + depth * ray;
-    if (abs(dist) < EPS) break;
+
+  // marching loop
+  float distance = 0.0; // レイとオブジェクト間の最短距離
+  float rLen = 0.0;     // レイに継ぎ足す長さ
+  vec3  rPos = cPos;    // レイの先端位置
+  for(int i = 0; i < 16; i++){
+      distance = getDistance(rPos);
+      rLen += distance;
+      rPos = cPos + ray * rLen;
   }
-  // 衝突判定の結果に応じたピクセル色の決定
-  vec3 lightDir = normalize(vec3(-3, 5, -2));
-  vec3 color;
-  if (abs(dist) < EPS) {
-    // 衝突した場合
-    vec3 normal = getNormal(dPos);
-    float diffuse = clamp(dot(lightDir, normal), 0.1, 1.0);
-    color = vec3(1.0, 1.0, 1.0) * diffuse;
-  } else {
-    // 衝突しなかった場合
-    color = vec3(1.0);
+
+  // hit check
+  if(abs(distance) < 0.001){
+      gl_FragColor = vec4(vec3(1.0), 1.0);
+  }else{
+      gl_FragColor = vec4(vec3(0.0), 1.0);
   }
-  gl_FragColor = vec4(color, 1.0);
 }
