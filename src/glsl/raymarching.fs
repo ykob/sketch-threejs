61103bac780092ba904b42efcf10a738f4897f1c
yoichi kobayashi <info@tplh.net>
2016-03-08 23:01:07 +0900

    [modify] added object in ray marching.

--- src/glsl/raymarching.fs
+++ src/glsl/raymarching.fs
@@ -8,24 +8,24 @@ const float PI = 3.14159265;
 const float angle = 60.0;
 const float fov = angle * 0.5 * PI / 180.0;
 
-const float sphere_size = 1.0;
 const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
 
 vec3 trans(vec3 p){
   return mod(p, 16.0) - 8.0;
 }
 
-float getDistance(vec3 p) {
-  return length(trans(p)) - sphere_size;
+float getDistanceSphere(vec3 p, float r) {
+  return length(p) - r;
 }
 
-vec3 getNormal(vec3 p){
-  float d = 0.0001;
-  return normalize(vec3(
-    getDistance(p + vec3(  d, 0.0, 0.0)) - getDistance(p + vec3( -d, 0.0, 0.0)),
-    getDistance(p + vec3(0.0,   d, 0.0)) - getDistance(p + vec3(0.0,  -d, 0.0)),
-    getDistance(p + vec3(0.0, 0.0,   d)) - getDistance(p + vec3(0.0, 0.0,  -d))
-  ));
+float getDistanceBox(vec3 p, vec3 size) {
+  return length(max(abs(p) - size, 0.0));
+}
+
+float getDistance(vec3 p) {
+	float d_sphere = getDistanceSphere(p, 1.3);
+	float d_box = getDistanceBox(p, vec3(1.0));
+	return max(d_box, -d_sphere);
 }
 
 void main() {
@@ -33,7 +33,7 @@ void main() {
   vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
 
   // camera
-  vec3 cPos = vec3(0.0, 0.0, 0.0);
+  vec3 cPos = vec3(cos(time / 50.0) * 1.5, sin(time / 50.0) * 1.5, 10.0);
   vec3 cDir = vec3(0.0, 0.0, -1.0);
   vec3 cUp  = vec3(0.0, 1.0, 0.0);
   vec3 cSide = cross(cDir, cUp);
@@ -46,18 +46,16 @@ void main() {
   float distance = 0.0; // レイとオブジェクト間の最短距離
   float rLen = 0.0;     // レイに継ぎ足す長さ
   vec3  rPos = cPos;    // レイの先端位置
-  for(int i = 0; i < 256; i++){
+  for(int i = 0; i < 128; i++){
       distance = getDistance(rPos);
       rLen += distance;
-      rPos = cPos + vec3(mouse * 3.0, 0.0) + ray * rLen;
+      rPos = cPos + ray * rLen;
   }
 
   // hit check
   if(abs(distance) < 0.001){
-    vec3 normal = getNormal(rPos);
-    float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
-    gl_FragColor = vec4(vec3(diff), 1.0);
+    gl_FragColor = vec4(vec3(1.0), 1.0);
   }else{
-      gl_FragColor = vec4(vec3(0.0), 1.0);
+    gl_FragColor = vec4(vec3(0.0), 1.0);
   }
 }
