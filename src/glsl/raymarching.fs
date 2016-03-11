44f6f7fd56e8073e1447abda6a12c5cf64bdfafb
yoichi kobayashi <info@tplh.net>
2016-03-11 13:08:43 +0900

    [add] getNormal func.

--- src/glsl/raymarching.fs
+++ src/glsl/raymarching.fs
@@ -19,12 +19,21 @@ float getDistanceSphere(vec3 p, float r) {
 }
 
 float getDistanceBox(vec3 p, vec3 size) {
-  return length(max(abs(p) - size, 0.0));
+  return length(max(abs(trans(p)) - size, 0.0));
 }
 
-float getDistance(vec3 p) {
-  float d_sphere = getDistanceSphere(p, 1.0);
+float distanceFunc(vec3 p) {
+  float d_sphere = getDistanceBox(p, vec3(1.0));
   return d_sphere;
+}
+
+vec3 getNormal(vec3 p) {
+  const float d = 0.0001;
+  return normalize(vec3(
+    distanceFunc(p + vec3(d, 0.0, 0.0)) - distanceFunc(p + vec3(-d, 0.0, 0.0)),
+    distanceFunc(p + vec3(0.0, d, 0.0)) - distanceFunc(p + vec3(0.0, -d, 0.0)),
+    distanceFunc(p + vec3(0.0, 0.0, d)) - distanceFunc(p + vec3(0.0, 0.0, -d))
+  ));
 }
 
 void main() {
@@ -45,15 +54,16 @@ void main() {
   float distance = 0.0; // レイとオブジェクト間の最短距離
   float rLen = 0.0;     // レイに継ぎ足す長さ
   vec3  rPos = cPos;    // レイの先端位置
-  for(int i = 0; i < 32; i++){
-      distance = getDistance(rPos);
+  for(int i = 0; i < 64; i++){
+      distance = distanceFunc(rPos);
       rLen += distance;
       rPos = cPos + ray * rLen;
   }
 
   // hit check
+  vec3 normal = getNormal(rPos);
   if(abs(distance) < 0.001){
-    gl_FragColor = vec4(vec3(1.0), 1.0);
+    gl_FragColor = vec4(normal, 1.0);
   }else{
     gl_FragColor = vec4(vec3(0.0), 1.0);
   }
