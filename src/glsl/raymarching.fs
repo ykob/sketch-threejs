84a21c2ce472d3ac7060b4c248a1d672ac2700a1
yoichi kobayashi <info@tplh.net>
2016-04-13 22:07:47 +0900

    clean up.

--- src/glsl/raymarching.fs
+++ src/glsl/raymarching.fs
@@ -7,9 +7,12 @@ uniform vec2 resolution;
 varying mat4 m_matrix;
 
 // uniform vec3 cPos;
-// uniform vec3 cDir;
-// uniform vec3 cUp;
 
+const vec3 cPos = vec3(0.0, 0.0, 10.0);
+const vec3 cDir = vec3(0.0, 0.0, -1.0);
+const vec3 cUp  = vec3(0.0, 1.0, 0.0);
+const vec3 cSide = cross(cDir, cUp);
+const float targetDepth = 1.8;
 const vec3 lightDir = vec3(0.577, -0.577, 0.577);
 
 #pragma glslify: hsv2rgb = require(./modules/hsv2rgb)
@@ -53,12 +56,6 @@ vec3 getNormal(vec3 p) {
 
 void main() {
   vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-
-  vec3 cPos = vec3(0.0, 0.0, 10.0);
-  vec3 cDir = vec3(0.0, 0.0, -1.0);
-  vec3 cUp  = vec3(0.0, 1.0, 0.0);
-  vec3 cSide = cross(cDir, cUp);
-  float targetDepth = 1.8;
 
   vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir * targetDepth);
 
