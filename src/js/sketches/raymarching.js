56e7e87bc0bd157745cfb4c09a1b9f7feadc617d
yoichi kobayashi <info@tplh.net>
2016-04-17 01:28:21 +0900

    [add] background obj and light in sketch of ray marching.

--- src/js/sketches/raymarching.js
+++ src/js/sketches/raymarching.js
@@ -1,5 +1,6 @@
 var Util = require('../modules/util');
 var glslify = require('glslify');
+var HemiLight = require('../modules/hemiLight');
 // var vs = glslify('../sketches/points.vs');
 // var fs = glslify('../sketches/points.fs');
 var vs = glslify('../../glsl/raymarching.vs');
@@ -9,7 +10,18 @@ var exports = function(){
   var Sketch = function(scene, camera) {
     this.init(scene, camera);
   };
-  var plane_geometry = new THREE.PlaneBufferGeometry(5.0, 5.0);
+  var light = new HemiLight();
+
+  var createBackground = function() {
+    var geometry = new THREE.SphereGeometry(80, 32, 32);
+    var material = new THREE.MeshPhongMaterial({
+      side: THREE.DoubleSide,
+      // shading: THREE.FlatShading
+    });
+    return new THREE.Mesh(geometry, material);
+  };
+
+  var plane_geometry = new THREE.PlaneBufferGeometry(6.0, 6.0);
   var plane_material = new THREE.ShaderMaterial({
     uniforms: {
       time: {
@@ -26,13 +38,19 @@ var exports = function(){
     transparent: true
   });
   var plane = new THREE.Mesh(plane_geometry, plane_material);
+  var bg = createBackground();
 
   Sketch.prototype = {
     init: function(scene, camera) {
       scene.add(plane);
+      scene.add(bg);
 
-      camera.range = 20;
-      camera.rad1_base = Util.getRadian(0);
+      light.rad1 = Util.getRadian(90);
+      light.init(0x777777, 0x111111);
+      scene.add(light.obj);
+
+      camera.range = 24;
+      camera.rad1_base = Util.getRadian(30);
       camera.rad1 = camera.rad1_base;
       camera.rad2 = Util.getRadian(90);
       camera.setPositionSpherical();
@@ -41,6 +59,10 @@ var exports = function(){
       plane.geometry.dispose();
       plane.material.dispose();
       scene.remove(plane);
+      bg.geometry.dispose();
+      bg.material.dispose();
+      scene.remove(bg);
+      scene.remove(light.obj);
       camera.range = 1000;
     },
     render: function(scene, camera) {
