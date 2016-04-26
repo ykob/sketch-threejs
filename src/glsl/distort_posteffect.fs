e45a8351df6f03a079694928e751e499d766acfb
yoichi kobayashi <info@tplh.net>
2016-04-26 20:29:22 +0900

    [modify] Increase accuracy of mosaic filter.

--- src/glsl/distort_posteffect.fs
+++ src/glsl/distort_posteffect.fs
@@ -3,17 +3,16 @@ uniform sampler2D texture;
 
 varying vec2 vUv;
 
-const float scale = 12.0;
+const float mosaic = 16.0;
 
 void main() {
   vec4 color = vec4(0.0);
-  float offsetX = mod(gl_FragCoord.x, scale) / resolution.x;
-  float offsetY = mod(gl_FragCoord.y, scale) / resolution.y;
+  vec2 offset = vec2(mod(gl_FragCoord.x, mosaic), mod(gl_FragCoord.y, mosaic));
 
-  for(float x = 0.0; x <= scale - 1.0; x += 1.0){
-    for(float y = 0.0; y <= scale - 1.0; y += 1.0){
-      color += texture2D(texture, vUv - vec2(offsetX, offsetY));
+  for (float x = 0.0; x <= mosaic - 1.0; x += 1.0){
+    for (float y = 0.0; y <= mosaic - 1.0; y += 1.0){
+      color += texture2D(texture, vUv - (offset + vec2(x, y)) / resolution);
     }
   }
-  gl_FragColor = color / pow(scale, 2.0);
+  gl_FragColor = color / pow(mosaic, 2.0);
 }
