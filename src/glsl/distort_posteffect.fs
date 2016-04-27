8ad7aab4b68946dc32888ea4dbef2008d5fc775d
yoichi kobayashi <info@tplh.net>
2016-04-27 10:21:55 +0900

    [modify] tweaked mosaic effect.

--- src/glsl/distort_posteffect.fs
+++ src/glsl/distort_posteffect.fs
@@ -9,8 +9,8 @@ void main() {
   vec4 color = vec4(0.0);
   vec2 offset = vec2(mod(gl_FragCoord.x, mosaic), mod(gl_FragCoord.y, mosaic));
 
-  for (float x = 0.0; x <= mosaic - 1.0; x += 1.0){
-    for (float y = 0.0; y <= mosaic - 1.0; y += 1.0){
+  for (float x = 0.0; x < mosaic; x++){
+    for (float y = 0.0; y < mosaic; y++){
       color += texture2D(texture, vUv - (offset + vec2(x, y)) / resolution);
     }
   }
