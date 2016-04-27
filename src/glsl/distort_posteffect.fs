6afc103701bbee7d9ac65b65adaef29d88e2d9ea
yoichi kobayashi <info@tplh.net>
2016-04-27 11:40:46 +0900

    [modify] change mosaic filter to blur filter.

--- src/glsl/distort_posteffect.fs
+++ src/glsl/distort_posteffect.fs
@@ -1,18 +1,17 @@
 uniform vec2 resolution;
 uniform sampler2D texture;
 
+const float blur = 16.0;
+
 varying vec2 vUv;
 
-const float mosaic = 16.0;
-
 void main() {
-  vec4 color = vec4(0.0);
-  vec2 offset = vec2(mod(gl_FragCoord.x, mosaic), mod(gl_FragCoord.y, mosaic));
+    vec4 color = vec4(0.0);
 
-  for (float x = 0.0; x < mosaic; x++){
-    for (float y = 0.0; y < mosaic; y++){
-      color += texture2D(texture, vUv - (offset + vec2(x, y)) / resolution);
+    for (float x = 0.0; x < blur; x++){
+      for (float y = 0.0; y < blur; y++){
+        color += texture2D(texture, vUv - (vec2(x, y) - vec2(blur / 2.0)) / resolution);
+      }
     }
-  }
-  gl_FragColor = color / pow(mosaic, 2.0);
+    gl_FragColor = color / pow(blur, 2.0);
 }
