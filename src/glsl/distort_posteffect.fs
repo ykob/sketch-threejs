ac7f12723ea5daa9875dab5783d684fbdbe22900
yoichi kobayashi <info@tplh.net>
2016-04-28 19:10:18 +0900

    [add] change blur filter to displace rgb.

--- src/glsl/distort_posteffect.fs
+++ src/glsl/distort_posteffect.fs
@@ -6,12 +6,8 @@ const float blur = 16.0;
 varying vec2 vUv;
 
 void main() {
-    vec4 color = vec4(0.0);
-
-    for (float x = 0.0; x < blur; x++){
-      for (float y = 0.0; y < blur; y++){
-        color += texture2D(texture, vUv - (vec2(x, y) - vec2(blur / 2.0)) / resolution);
-      }
-    }
-    gl_FragColor = color / pow(blur, 2.0);
+  float r = texture2D(texture, vUv).r;
+  float g = texture2D(texture, vUv + vec2(-50.0, 0.0) / resolution).g;
+  float b = texture2D(texture, vUv + vec2(50.0, 0.0) / resolution).b;
+  gl_FragColor = vec4(r, g, b, 1.0);
 }
