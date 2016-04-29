39f081d73273e58ede008c3318a45b4642ca0537
yoichi kobayashi <info@tplh.net>
2016-04-30 01:02:38 +0900

    [add] noise effect when click window in distort sketch.

--- src/glsl/distort_posteffect.fs
+++ src/glsl/distort_posteffect.fs
@@ -7,10 +7,13 @@ const float blur = 16.0;
 
 varying vec2 vUv;
 
+#pragma glslify: snoise2 = require(glsl-noise/simplex/2d)
+
 void main() {
   float diff = 250.0 * length(acceleration);
-  float r = texture2D(texture, vUv).r;
-  float g = texture2D(texture, vUv + vec2(cos(time), sin(time)) * diff / resolution).g;
-  float b = texture2D(texture, vUv + vec2(cos(-time), sin(-time)) * diff / resolution).b;
+  float noize = snoise2(vUv * 10000.0 - time) * 0.01 * diff;
+  float r = texture2D(texture, vUv).r + noize;
+  float g = texture2D(texture, vUv + vec2(cos(time), sin(time)) * diff / resolution).g + noize;
+  float b = texture2D(texture, vUv + vec2(cos(-time), sin(-time)) * diff / resolution).b + noize;
   gl_FragColor = vec4(r, g, b, 1.0);
 }
