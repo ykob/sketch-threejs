a20acfbb0881d811b42d942f7d69eec02d175b4a
Yoichi Kobayashi <info@tplh.net>
2016-05-20 02:20:12 +0900

    [wip] update render function in PhysicsRenderer class.

--- src/js/sketches/gpgpu.js
+++ src/js/sketches/gpgpu.js
@@ -9,9 +9,27 @@ var exports = function(){
 
   var PhysicsRenderer = function(length) {
     this.length = Math.pow(length, 2);
-    this.scene = new THREE.Scene();
+    this.acceleration_scene = new THREE.Scene();
+    this.velocity_scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
-    this.mesh = new THREE.Mesh(
+    this.acceleration_mesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(2, 2),
+      new THREE.ShaderMaterial({
+        uniforms: {
+          time: {
+            type: 'f',
+            value: 0,
+          },
+          acceleration: {
+            type: 't',
+            value: null,
+          },
+        },
+        vertexShader: glslify('../../glsl/physics_renderer_acceleration.vs'),
+        fragmentShader: glslify('../../glsl/physics_renderer_acceleration.fs'),
+      })
+    );
+    this.velocity_mesh = new THREE.Mesh(
       new THREE.PlaneGeometry(2, 2),
       new THREE.ShaderMaterial({
         uniforms: {
@@ -28,27 +46,31 @@ var exports = function(){
             value: null,
           },
         },
-        vertexShader: glslify('../../glsl/physics_renderer.vs'),
-        fragmentShader: glslify('../../glsl/physics_renderer.fs'),
+        vertexShader: glslify('../../glsl/physics_renderer_velocity.vs'),
+        fragmentShader: glslify('../../glsl/physics_renderer_velocity.fs'),
       })
     );
-    this.velocity = [
-      THREE.WebGLRenderTarget(length, length),
-      THREE.WebGLRenderTarget(length, length),
-    ];
     this.acceleration = [
       THREE.WebGLRenderTarget(length, length),
       THREE.WebGLRenderTarget(length, length),
     ];
+    this.velocity = [
+      THREE.WebGLRenderTarget(length, length),
+      THREE.WebGLRenderTarget(length, length),
+    ];
+    this.target_index = 0;
 
-    this.scene.add(this.mesh);
+    this.acceleration_scene.add(this.acceleration_mesh);
+    this.velocity_scene.add(this.velocity_mesh);
   };
   PhysicsRenderer.prototype = {
     render: function(renderer) {
-      this.mesh.material.uniforms.velocity.value = this.velocity[0];
-      this.mesh.material.uniforms.acceleration.value = this.acceleration[0];
-      renderer.render(this.scene, this.camera, this.velocity[1]);
-      renderer.render(this.scene, this.camera, this.acceleration[1]);
+      this.acceleration_mesh.material.uniforms.acceleration.value = this.acceleration[Math.abs(target_index - 1)];
+      renderer.render(this.acceleration_scene, this.camera, this.acceleration[target_index]);
+      this.velocity_mesh.material.uniforms.acceleration.value = this.acceleration[target_index];
+      this.velocity_mesh.material.uniforms.velocity.value = this.velocity[Math.abs(target_index - 1)];
+      renderer.render(this.velocity_scene, this.camera, this.velocity[target_index]);
+      target_index = Math.abs(target_index - 1);
     },
     resize: function(length) {
       this.length = Math.pow(length, 2);
