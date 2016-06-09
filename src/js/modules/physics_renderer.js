e947776f2c4b1e6a6524776679ef8d08f56cb559
yoichi kobayashi <info@tplh.net>
2016-06-09 17:35:19 +0900

    [modify] mesh creation func summarized in one.

--- src/js/modules/physics_renderer.js
+++ src/js/modules/physics_renderer.js
@@ -17,35 +17,13 @@ var exports = function(){
       new THREE.WebGLRenderTarget(length, length, this.option),
       new THREE.WebGLRenderTarget(length, length, this.option),
     ];
-    this.acceleration_mesh = new THREE.Mesh(
-      new THREE.PlaneBufferGeometry(2, 2),
-      new THREE.ShaderMaterial({
-        uniforms: {
-          acceleration: {
-            type: 't',
-            value: new THREE.Texture(),
-          },
-        },
-        vertexShader: glslify('../../glsl/physics_renderer_acceleration.vs'),
-        fragmentShader: glslify('../../glsl/physics_renderer_acceleration.fs'),
-      })
+    this.acceleration_mesh = this.createMesh(
+      glslify('../../glsl/physics_renderer_acceleration.vs'),
+      glslify('../../glsl/physics_renderer_acceleration.fs')
     );
-    this.velocity_mesh = new THREE.Mesh(
-      new THREE.PlaneBufferGeometry(2, 2),
-      new THREE.ShaderMaterial({
-        uniforms: {
-          velocity: {
-            type: 't',
-            value: new THREE.Texture(),
-          },
-          acceleration: {
-            type: 't',
-            value: new THREE.Texture(),
-          },
-        },
-        vertexShader: glslify('../../glsl/physics_renderer_velocity.vs'),
-        fragmentShader: glslify('../../glsl/physics_renderer_velocity.fs'),
-      })
+    this.velocity_mesh = this.createMesh(
+      glslify('../../glsl/physics_renderer_velocity.vs'),
+      glslify('../../glsl/physics_renderer_velocity.fs')
     );
     this.target_index = 0;
 
@@ -55,8 +33,28 @@ var exports = function(){
     this.velocity_scene.add(this.velocity_mesh);
   };
   PhysicsRenderer.prototype = {
+    createMesh(vs, fs) {
+      return new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(2, 2),
+        new THREE.ShaderMaterial({
+          uniforms: {
+            velocity: {
+              type: 't',
+              value: new THREE.Texture(),
+            },
+            acceleration: {
+              type: 't',
+              value: new THREE.Texture(),
+            },
+          },
+          vertexShader: vs,
+          fragmentShader: fs,
+        })
+      );
+    },
     render: function(renderer) {
       this.acceleration_mesh.material.uniforms.acceleration.value = this.acceleration[Math.abs(this.target_index - 1)];
+      this.acceleration_mesh.material.uniforms.velocity.value = this.velocity[Math.abs(this.target_index)];
       renderer.render(this.acceleration_scene, this.camera, this.acceleration[this.target_index]);
       this.velocity_mesh.material.uniforms.acceleration.value = this.acceleration[this.target_index];
       this.velocity_mesh.material.uniforms.velocity.value = this.velocity[Math.abs(this.target_index - 1)];
