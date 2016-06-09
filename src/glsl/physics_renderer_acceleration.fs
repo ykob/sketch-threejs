37f5a55472a74819e0b6874c40556e9b294d15e4
yoichi kobayashi <info@tplh.net>
2016-06-09 17:37:55 +0900

    [add] apply hook and drag function in shader, and some tweak.

--- src/glsl/physics_renderer_acceleration.fs
+++ src/glsl/physics_renderer_acceleration.fs
@@ -1,10 +1,20 @@
 uniform float time;
+uniform sampler2D velocity;
 uniform sampler2D acceleration;
 
 varying vec2 vUv;
 
+vec3 applyDrag(vec3 a, float value) {
+  return normalize(a * -1.0) * length(a) * value;
+}
+
+vec3 applyHook(vec3 v, vec3 anchor, float rest_length, float k) {
+  return normalize(v - anchor) * (-1.0 * k * (length(v - anchor) - rest_length));
+}
+
 void main(void) {
-  vec3 gravity = vec3(0.0, 0.02, 0.0);
-  vec3 update_position = texture2D(acceleration, vUv).xyz + gravity;
-  gl_FragColor = vec4(update_position, 1.0);
+  vec3 v = texture2D(velocity, vUv).xyz;
+  vec3 a = texture2D(acceleration, vUv).xyz;
+  vec3 anchor = vec3(0.0, 1.0, 0.0);
+  gl_FragColor = vec4(a + applyHook(v, anchor, 0.0, 0.1), 1.0);
 }
