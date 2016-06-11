32382091646298fb82459f1ed7848c217a5d7c5e
yoichi kobayashi <info@tplh.net>
2016-06-12 00:02:53 +0900

    [add] attract function in physics renderer.

--- src/glsl/physics_renderer_acceleration.fs
+++ src/glsl/physics_renderer_acceleration.fs
@@ -6,18 +6,21 @@ varying vec2 vUv;
 
 #define PRECISION 0.000001
 
-vec3 applyDrag(vec3 a, float value) {
+vec3 drag(vec3 a, float value) {
   return normalize(a * -1.0 + PRECISION) * length(a) * value;
 }
 
-vec3 applyHook(vec3 v, vec3 anchor, float rest_length, float k) {
+vec3 hook(vec3 v, vec3 anchor, float rest_length, float k) {
   return normalize(v - anchor + PRECISION) * (-1.0 * k * (length(v - anchor) - rest_length));
+}
+
+vec3 attract(vec3 v1, vec3 v2, float m1, float m2, float g) {
+  return g * m1 * m2 / pow(clamp(length(v2 - v1), 8.0, 16.0), 2.0) * normalize(v2 - v1 + PRECISION);
 }
 
 void main(void) {
   vec3 v = texture2D(velocity, vUv).xyz;
   vec3 a = texture2D(acceleration, vUv).xyz;
   vec3 anchor = vec3(0.0, 0.0, 0.0);
-  vec3 update_a = a + applyHook(v, anchor, 100.0, 0.1);
-  gl_FragColor = vec4(update_a + applyDrag(update_a, 0.1), 1.0);
+  gl_FragColor = vec4(a + attract(v, anchor, 10.0, 1.0, 2.0), 1.0);
 }
