00551dd48eba50b7700b3463d54bf7dd96b283a5
yoichi kobayashi <info@tplh.net>
2016-06-26 00:16:27 +0900

    [modify] add flat shading to buffer object in sketch hole.

--- src/glsl/sketch/hole/object.fs
+++ src/glsl/sketch/hole/object.fs
@@ -1,3 +1,15 @@
+struct DirectionalLight {
+  vec3 color;
+  vec3 direction;
+};
+uniform DirectionalLight directionalLights[1];
+
+varying vec3 vPosition;
+varying mat4 vInvertMatrix;
+
 void main() {
+  vec3 normal = normalize(cross(dFdx(vPosition), dFdy(vPosition)));
+  vec3 inv_light = normalize(vInvertMatrix * vec4(directionalLights[0].direction, 1.0)).xyz;
+  float diff = (dot(normal, inv_light) + 1.0) / 2.0;
   gl_FragColor = vec4(1.0);
 }
--- src/glsl/sketch/hole/object.vs
+++ src/glsl/sketch/hole/object.vs
@@ -1,5 +1,12 @@
 uniform float time;
 
+varying vec3 vPosition;
+varying mat4 vInvertMatrix;
+
+#pragma glslify: inverse = require(glsl-inverse);
+
 void main() {
+  vPosition = position;
+  vInvertMatrix = inverse(modelMatrix);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
