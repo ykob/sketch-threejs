70cf59c854d110dd9474f859b8aa16b5595bffcf
yoichi kobayashi <info@tplh.net>
2017-01-17 18:54:13 +0900

    [modify] copy old js file.

---
+++ src/js/modules/util.js
@@ -0,0 +1,17 @@
+export default function() {
+  getRandomInt: function(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  getDegree: function(radian) {
+    return radian / Math.PI * 180;
+  },
+  getRadian: function(degrees) {
+    return degrees * Math.PI / 180;
+  },
+  getPolarCoord: function(rad1, rad2, r) {
+    var x = Math.cos(rad1) * Math.cos(rad2) * r;
+    var z = Math.cos(rad1) * Math.sin(rad2) * r;
+    var y = Math.sin(rad1) * r;
+    return new THREE.Vector3(x, y, z);
+  }
+}
