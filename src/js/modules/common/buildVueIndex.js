abf1aa750e9e19f4080cbac8c92be08375fdb0b8
yoichi kobayashi <info@tplh.net>
2017-02-01 20:32:11 +0900

    [add] toggle effects to sketch index element.

--- src/js/modules/common/buildVueIndex.js
+++ src/js/modules/common/buildVueIndex.js
@@ -2,15 +2,16 @@ export default function(_this) {
   return new Vue({
     el: '#index',
     data: {
-      isOpened: false
+      isOpened: -1
     },
     methods: {
       toggle: function() {
-        this.isOpened = !this.isOpened;
-        if (this.isOpened) {
-          _this.open();
-        } else {
+        if (this.isOpened == 1) {
+          this.isOpened = 0;
           _this.close();
+        } else {
+          this.isOpened = 1;
+          _this.open();
         }
       }
     }
