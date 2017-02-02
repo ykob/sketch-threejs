9b4ae19d4146c16e799a72eb87a9b55c86a4be56
yoichi kobayashi <info@tplh.net>
2017-02-02 12:09:44 +0900

    [modify] fede in effect in the ground object.

--- src/glsl/index/ground.fs
+++ src/glsl/index/ground.fs
@@ -8,7 +8,7 @@ const float duration = 6.0;
 const float delay = 1.0;
 
 void main() {
-  float now = (time - delay) / duration;
-  float opacity = 1.0 - smoothstep(0.4, 1.0, (vPosition.y + 512.0) / 1024.0);
-  gl_FragColor = vec4(vec3(0.4), opacity * (now - length(vPosition.xy) / vec2(1024.0, 512.0) * 0.2));
+  float now = min((time - delay) / duration, 1.0);
+  float opacity = (1.0 - (vPosition.y + 512.0) / 1024.0);
+  gl_FragColor = vec4(vec3(0.4), opacity * (now * 2.0 - length(vPosition.xy) / vec2(1024.0, 512.0)));
 }
