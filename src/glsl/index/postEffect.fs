57dbeaa4fa11278209fa3b3eb9b4ec4755f501eb
yoichi kobayashi <info@tplh.net>
2017-02-08 13:16:32 +0900

    [modify] whitenoise on index.

--- src/glsl/index/postEffect.fs
+++ src/glsl/index/postEffect.fs
@@ -10,7 +10,7 @@ varying vec2 vUv;
 #pragma glslify: random = require(glsl-util/random);
 
 void main() {
-  float whiteNoise = random(vUv.xy + time) * 0.1 - 0.1;
+  float whiteNoise = random(vUv.xy * time) * 0.1 - 0.1;
   float r = texture2D(texture, vUv - vec2(2.0, 0.0) / resolution).r;
   float g = texture2D(texture, vUv).g;
   float b = texture2D(texture, vUv + vec2(2.0, 0.0) / resolution).b;
