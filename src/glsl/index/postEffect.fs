40cd40033f554e214b4ac217f1ac8bd9cfb755f9
yoichi kobayashi <info@tplh.net>
2017-02-10 23:50:17 +0900

    [add] vignette effects.

--- src/glsl/index/postEffect.fs
+++ src/glsl/index/postEffect.fs
@@ -6,13 +6,28 @@ uniform sampler2D texture;
 
 varying vec2 vUv;
 
+const float duration = 8.0;
+const float delay = 4.0;
+
 #pragma glslify: cnoise3 = require(glsl-noise/classic/3d)
 #pragma glslify: random = require(glsl-util/random);
+#pragma glslify: convertHsvToRgb = require(glsl-util/convertHsvToRgb);
 
 void main() {
+  float now = clamp((time - delay) / duration, 0.0, 1.0);
+
+  // ホワイトノイズ
   float whiteNoise = random(vUv.xy * time) * 0.1 - 0.1;
+
+  // ヴィネット
+  float vignette = smoothstep(0.6, 1.4, length(vUv * 2.0 - 1.0 - vec2(0.0, 0.25)));
+  float vignetteNoise = cnoise3(vec3((vUv * resolution * 0.004), time)) * 0.6;
+  vec3 vignetteColor = convertHsvToRgb(vec3(0.5 + (vUv.x + vUv.y) / 40.0 + time * 0.1, 0.4, 1.0));
+
+  // RGBズレ
   float r = texture2D(texture, vUv - vec2(2.0, 0.0) / resolution).r;
   float g = texture2D(texture, vUv).g;
   float b = texture2D(texture, vUv + vec2(2.0, 0.0) / resolution).b;
-  gl_FragColor = vec4(vec3(r, g, b) + whiteNoise, 1.0);
+
+  gl_FragColor = vec4((vec3(r, g, b) + whiteNoise) + (pow(vignette + vignetteNoise, 3.0) * vignetteColor), 1.0);
 }
