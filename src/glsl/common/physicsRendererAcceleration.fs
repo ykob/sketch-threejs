815a97eb33ac6e22731d014e7a0ea55e480d96b2
yoichi kobayashi <info@tplh.net>
2017-02-19 22:52:25 +0900

    tweak.

--- src/glsl/common/physicsRendererAcceleration.fs
+++ src/glsl/common/physicsRendererAcceleration.fs
@@ -7,17 +7,7 @@ varying vec2 vUv;
 
 #define PRECISION 0.000001
 
-vec3 drag(vec3 a, float value) {
-  return normalize(a * -1.0 + PRECISION) * length(a) * value;
-}
-
-vec3 hook(vec3 v, vec3 anchor, float rest_length, float k) {
-  return normalize(v - anchor + PRECISION) * (-1.0 * k * (length(v - anchor) - rest_length));
-}
-
-vec3 attract(vec3 v1, vec3 v2, float m1, float m2, float g) {
-  return g * m1 * m2 / pow(clamp(length(v2 - v1), 5.0, 30.0), 2.0) * normalize(v2 - v1 + PRECISION);
-}
+#pragma glslify: drag = require(glsl-force/drag)
 
 void main(void) {
   vec3 v = texture2D(velocity, vUv).xyz;
