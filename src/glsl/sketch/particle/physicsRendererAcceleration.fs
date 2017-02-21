64527903073a9f10745539adba0f795d2d39e37a
yoichi kobayashi <info@tplh.net>
2017-02-21 11:37:53 +0900

    [add] compute acceleration for sketch particle with drag function.

--- src/glsl/sketch/particle/physicsRendererAcceleration.fs
+++ src/glsl/sketch/particle/physicsRendererAcceleration.fs
@@ -6,11 +6,13 @@ uniform float time;
 varying vec2 vUv;
 
 #pragma glslify: cnoise3 = require(glsl-noise/classic/3d)
+#pragma glslify: drag = require(glsl-force/drag)
 
 void main(void) {
   vec3 v = texture2D(velocity, vUv).xyz;
   vec3 a = texture2D(acceleration, vUv).xyz;
-  float fx = cnoise3(vec3(0.0, v.y / 100.0 + time, v.z / 100.0)) * 8.0;
-  float fy = cnoise3(vec3(v.x / 100.0 + time, 0.0, v.z / 100.0)) * 8.0;
-  gl_FragColor = vec4(vec3(fx, fy, 0.0), 1.0);
+  vec3 d = drag(a, 0.8);
+  float fx = cnoise3(vec3(time * 10.0, v.y / 100.0, v.z / 100.0));
+  float fy = cnoise3(vec3(v.x / 100.0, time * 10.0, v.z / 100.0));
+  gl_FragColor = vec4(a + d + vec3(fx, fy, 0.0), 1.0);
 }
