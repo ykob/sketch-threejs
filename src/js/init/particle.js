06c1880be3d39a62aa4fcde3ecf314a9fd67112f
yoichi kobayashi <info@tplh.net>
2017-02-23 22:49:35 +0900

    [add] modularized Points class.

--- src/js/init/particle.js
+++ src/js/init/particle.js
@@ -1,5 +1,5 @@
 import normalizeVector2 from '../modules/common/normalizeVector2';
-import PhysicsRenderer from '../modules/common/PhysicsRenderer';
+import Points from '../modules/sketch/particle/Points';
 
 const debounce = require('js-util/debounce');
 
@@ -22,87 +22,8 @@ export default function() {
   // process for this sketch.
   //
 
-  const glslify = require('glslify');
-
-  class OctahedronPoints {
-    constructor() {
-      this.uniforms = {
-        time: {
-          type: 'f',
-          value: 0
-        },
-        velocity: {
-          type: 't',
-          value: null
-        },
-        acceleration: {
-          type: 't',
-          value: null
-        }
-      };
-      this.physicsRenderer = null;
-      this.vectorTouchMove = new THREE.Vector2(0, 0);
-      this.vectorTouchMoveDiff = new THREE.Vector2(0, 0);
-      this.obj = this.createObj();
-    }
-    createObj() {
-      const detail = (window.innerWidth > 768) ? 7 : 6;
-      const geometry = new THREE.OctahedronBufferGeometry(400, detail);
-      const verticesBase = geometry.attributes.position.array;
-      const vertices = [];
-      for (var i = 0; i < verticesBase.length; i+= 3) {
-        vertices[i + 0] = verticesBase[i + 0] + (Math.random() * 2 - 1) * 400;
-        vertices[i + 1] = verticesBase[i + 1] + (Math.random() * 2 - 1) * 400;
-        vertices[i + 2] = verticesBase[i + 2] + (Math.random() * 2 - 1) * 400;
-      }
-      this.physicsRenderer = new PhysicsRenderer(
-        glslify('../../glsl/sketch/particle/physicsRendererAcceleration.vs'),
-        glslify('../../glsl/sketch/particle/physicsRendererAcceleration.fs'),
-        glslify('../../glsl/sketch/particle/physicsRendererVelocity.vs'),
-        glslify('../../glsl/sketch/particle/physicsRendererVelocity.fs')
-      );
-      this.physicsRenderer.init(renderer, vertices);
-      this.physicsRenderer.mergeAUniforms({
-        vTouchMove: {
-          type: 'v2',
-          value: this.vectorTouchMoveDiff
-        }
-      });
-      this.uniforms.velocity.value = this.physicsRenderer.getCurrentVelocity();
-      this.uniforms.acceleration.value = this.physicsRenderer.getCurrentAcceleration();
-      geometry.addAttribute('uvVelocity', this.physicsRenderer.getBufferAttributeUv());
-      return new THREE.Points(
-        geometry,
-        new THREE.RawShaderMaterial({
-          uniforms: this.uniforms,
-          vertexShader: glslify('../../glsl/sketch/particle/OctahedronPoints.vs'),
-          fragmentShader: glslify('../../glsl/sketch/particle/octahedronPoints.fs'),
-          transparent: true,
-          depthWrite: false,
-          blending: THREE.AdditiveBlending,
-        })
-      )
-    }
-    render(time) {
-      this.physicsRenderer.render(renderer, time);
-      this.uniforms.time.value += time;
-    }
-    touchStart(v) {
-      this.vectorTouchMove.copy(v);
-    }
-    touchMove(v) {
-      this.vectorTouchMoveDiff.set(
-        v.x - this.vectorTouchMove.x,
-        v.y - this.vectorTouchMove.y
-      );
-      this.vectorTouchMove.copy(v);
-    }
-    touchEnd() {
-      this.vectorTouchMove.set(0, 0);
-      this.vectorTouchMoveDiff.set(0, 0);
-    }
-  }
-  const points = new OctahedronPoints();
+  const points = new Points();
+  points.init(renderer);
 
   //
   // common process
@@ -116,7 +37,7 @@ export default function() {
   }
   const render = () => {
     const time = clock.getDelta();
-    points.render(time);
+    points.render(renderer, time);
     renderer.render(scene, camera);
   }
   const renderLoop = () => {
