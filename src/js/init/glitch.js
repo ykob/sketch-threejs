d709ff1ecd22d08c9a72178c66344d55a2147bef
yoichi kobayashi <info@tplh.net>
2017-04-19 15:42:13 +0900

    [add] base files for sketch glitch.

---
+++ src/js/init/glitch.js
@@ -0,0 +1,106 @@
+import normalizeVector2 from '../modules/common/normalizeVector2';
+
+const debounce = require('js-util/debounce');
+
+export default function() {
+  const canvas = document.getElementById('canvas-webgl');
+  const renderer = new THREE.WebGLRenderer({
+    antialias: false,
+    canvas: canvas,
+  });
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(45, document.body.clientWidth / window.innerHeight, 1, 10000);
+  const clock = new THREE.Clock();
+
+  const vectorTouchStart = new THREE.Vector2();
+  const vectorTouchMove = new THREE.Vector2();
+  const vectorTouchEnd = new THREE.Vector2();
+
+  let isDrag = false;
+
+  //
+  // process for this sketch.
+  //
+
+
+
+  //
+  // common process
+  //
+  const resizeWindow = () => {
+    canvas.width = document.body.clientWidth;
+    canvas.height = window.innerHeight;
+    camera.aspect = document.body.clientWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(document.body.clientWidth, window.innerHeight);
+  }
+  const render = () => {
+    renderer.render(scene, camera);
+  }
+  const renderLoop = () => {
+    render();
+    requestAnimationFrame(renderLoop);
+  }
+  const touchStart = (isTouched) => {
+    isDrag = true;
+  };
+  const touchMove = (isTouched) => {
+    if (isDrag) {}
+  };
+  const touchEnd = (isTouched) => {
+    isDrag = false;
+  };
+  const on = () => {
+    window.addEventListener('resize', debounce(() => {
+      resizeWindow();
+    }), 1000);
+    canvas.addEventListener('mousedown', function (event) {
+      event.preventDefault();
+      vectorTouchStart.set(event.clientX, event.clientY);
+      normalizeVector2(vectorTouchStart);
+      touchStart(false);
+    });
+    document.addEventListener('mousemove', function (event) {
+      event.preventDefault();
+      vectorTouchMove.set(event.clientX, event.clientY);
+      normalizeVector2(vectorTouchMove);
+      touchMove(false);
+    });
+    document.addEventListener('mouseup', function (event) {
+      event.preventDefault();
+      vectorTouchEnd.set(event.clientX, event.clientY);
+      normalizeVector2(vectorTouchEnd);
+      touchEnd(false);
+    });
+    canvas.addEventListener('touchstart', function (event) {
+      event.preventDefault();
+      vectorTouchStart.set(event.touches[0].clientX, event.touches[0].clientY);
+      normalizeVector2(vectorTouchStart);
+      touchStart(event.touches[0].clientX, event.touches[0].clientY, true);
+    });
+    canvas.addEventListener('touchmove', function (event) {
+      event.preventDefault();
+      vectorTouchMove.set(event.touches[0].clientX, event.touches[0].clientY);
+      normalizeVector2(vectorTouchMove);
+      touchMove(true);
+    });
+    canvas.addEventListener('touchend', function (event) {
+      event.preventDefault();
+      vectorTouchEnd.set(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+      normalizeVector2(vectorTouchEnd);
+      touchEnd(true);
+    });
+  }
+
+  const init = () => {
+    renderer.setSize(document.body.clientWidth, window.innerHeight);
+    renderer.setClearColor(0xeeeeee, 1.0);
+    camera.position.set(1000, 1000, 1000);
+    camera.lookAt(new THREE.Vector3());
+
+    on();
+    resizeWindow();
+    renderLoop();
+  }
+  init();
+}
