275e0c3af485c46088f7365ac59c8a7a6b39f305
yoichi kobayashi <info@tplh.net>
2017-04-24 00:44:47 +0900

    [modify] pattern of block noise.

--- src/glsl/sketch/glitch/postEffect.fs
+++ src/glsl/sketch/glitch/postEffect.fs
@@ -14,8 +14,8 @@ void main(void){
 
   float whiteNoise = (random(gl_FragCoord.xy + mod(time, 10.0)) * 2.0 - 1.0) * 0.4;
 
-  float noiseX = step((snoise3(vec3(0.0, gl_FragCoord.x / 500.0, time * 1000.0)) + 1.0) / 2.0, (sin(time) + 1.0) / 2.0 * 0.15 + 0.15);
-  float noiseY = step((snoise3(vec3(0.0, gl_FragCoord.y / 400.0, time * 400.0)) + 1.0) / 2.0, (sin(time) + 1.0) / 2.0 * 0.15 + 0.15);
+  float noiseX = step((snoise3(vec3(0.0, gl_FragCoord.x / 500.0, time * 1000.0)) + 1.0) / 2.0, 0.3);
+  float noiseY = step((snoise3(vec3(0.0, gl_FragCoord.y / 400.0, time * 400.0)) + 1.0) / 2.0, 0.3);
   float blockNoiseMask = noiseX * noiseY;
   vec4 blockNoise = texture2D(texture, 1.0 - vUv) * blockNoiseMask;
 
