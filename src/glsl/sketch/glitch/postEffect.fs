43b9255086d5ba6f72c171bf0c157ef7b5b91f6f
yoichi kobayashi <info@tplh.net>
2017-04-30 01:31:10 +0900

    [modify] clean up post effects.

--- src/glsl/sketch/glitch/postEffect.fs
+++ src/glsl/sketch/glitch/postEffect.fs
@@ -25,17 +25,17 @@ void main(void){
 
   float whiteNoise = (random(gl_FragCoord.xy + mod(time, 10.0)) * 2.0 - 1.0) * (0.2 + strength * 0.1);
 
-  float blockNoiseTime = floor(time * 20.0);
-  float noiseX = step((snoise3(vec3(0.0, gl_FragCoord.x / 480.0, blockNoiseTime * 200.0)) + 1.0) / 2.0, 0.1 + strength * 0.3);
-  float noiseY = step((snoise3(vec3(0.0, gl_FragCoord.y / 480.0, blockNoiseTime * 200.0)) + 1.0) / 2.0, 0.1 + strength * 0.2);
-  float blockNoiseMask = noiseX * noiseY;
-  float blockNoiseUvX = vUv.x + sin(blockNoiseTime * 200.0) * 0.2 + rgbWave;
-  float bnr = texture2D(texture, vec2(blockNoiseUvX + 6.0 / resolution.x, vUv.y)).r * blockNoiseMask;
-  float bng = texture2D(texture, vec2(blockNoiseUvX, vUv.y)).g * blockNoiseMask;
-  float bnb = texture2D(texture, vec2(blockNoiseUvX - 6.0 / resolution.x, vUv.y)).b * blockNoiseMask;
-  vec4 blockNoise = vec4(bnr, bng, bnb, 1.0);
+  float bnTime = floor(time * 20.0) * 200.0;
+  float noiseX = step((snoise3(vec3(0.0, gl_FragCoord.x / 480.0, bnTime)) + 1.0) / 2.0, 0.1 + strength * 0.3);
+  float noiseY = step((snoise3(vec3(0.0, gl_FragCoord.y / 480.0, bnTime)) + 1.0) / 2.0, 0.1 + strength * 0.2);
+  float bnMask = noiseX * noiseY;
+  float bnUvX = vUv.x + sin(bnTime) * 0.2 + rgbWave;
+  float bnR = texture2D(texture, vec2(bnUvX + 6.0 / resolution.x, vUv.y)).r * bnMask;
+  float bnG = texture2D(texture, vec2(bnUvX, vUv.y)).g * bnMask;
+  float bnB = texture2D(texture, vec2(bnUvX - 6.0 / resolution.x, vUv.y)).b * bnMask;
+  vec4 blockNoise = vec4(bnR, bnG, bnB, 1.0);
 
   float waveNoise = (sin(gl_FragCoord.y * 1.4) + 1.0) / 2.0 * (0.15 + strength * 0.2);
 
-  gl_FragColor = vec4(r, g, b, 1.0) * (1.0 - blockNoiseMask) + (whiteNoise + blockNoise - waveNoise);
+  gl_FragColor = vec4(r, g, b, 1.0) * (1.0 - bnMask) + (whiteNoise + blockNoise - waveNoise);
 }
