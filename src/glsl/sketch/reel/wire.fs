cc9bbe30574b8d97a56ee2a588611c7386f0a6e8
yoichi kobayashi <info@tplh.net>
2017-05-07 14:21:07 +0900

    [modify] color of wire.

--- src/glsl/sketch/reel/wire.fs
+++ src/glsl/sketch/reel/wire.fs
@@ -5,7 +5,9 @@ uniform vec3 cameraPosition;
 
 varying vec4 vPosition;
 
+const vec3 color = vec3(0.75);
+
 void main() {
   float opacity = length(cameraPosition) / length(cameraPosition - vPosition.xyz) - 0.5;
-  gl_FragColor = vec4(0.7, 0.7, 0.7, opacity);
+  gl_FragColor = vec4(color, opacity);
 }
