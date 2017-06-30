d2db7da6ad861303757698cf185e7b4d3dfa23d7
yoichi kobayashi <info@tplh.net>
2017-07-01 02:23:45 +0900

    [add] noise to circle animation.

--- src/glsl/sketch/puddle/puddle.fs
+++ src/glsl/sketch/puddle/puddle.fs
@@ -4,11 +4,14 @@ uniform float time;
 
 varying vec2 vUv;
 
+#pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
 void main() {
-  float t = mod(time, 1.0);
+  float t = mod(time, 2.0) / 2.0;
   vec2 pos = vUv * 2.0 - vec2(1.0);
-  float circleOut = clamp(ceil(min(t, 0.7) - length(pos)), 0.0, 1.0);
-  float circleIn = clamp(ceil(t - 0.3 - length(pos)), 0.0, 1.0);
-  float opacity = circleOut - circleIn;
+  float circleOut = smoothstep(0.0, 0.2, 1.0 - length(pos));
+  float circleIn = smoothstep(0.0, 0.2, 0.8 - length(pos));
+  float noise = (snoise3(vec3(pos, time)) + 1.0) / 2.0;
+  float opacity = circleOut - noise + circleIn;
   gl_FragColor = vec4(0.0, 0.0, 0.0, opacity);
 }
