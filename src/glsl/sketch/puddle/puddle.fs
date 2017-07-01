8799c6282a7e654bc696fef31a58f62feb2929ab
yoichi kobayashi <info@tplh.net>
2017-07-01 14:16:43 +0900

    [modify] animation of puddle object.

--- src/glsl/sketch/puddle/puddle.fs
+++ src/glsl/sketch/puddle/puddle.fs
@@ -12,11 +12,11 @@ void main() {
   vec2 pos = vUv * 2.0 - vec2(1.0);
   float circleOut = smoothstep(0.0, 0.5, 1.0 - length(pos));
   float circleIn = smoothstep(0.0, 0.5, 0.5 - length(pos));
-  float noise = (snoise3(vec3(pos * 1.6 + vec2(sin(time), cos(time)), time * 0.5)) + 1.0) / 2.0;
+  float noise = (snoise3(vec3(pos * 1.6 + vec2(sin(time), cos(time)), time)) + 1.0) / 2.0;
   float mask = circleOut * noise + circleIn;
   float opacity = 1.0 - step(mask, 0.3);
-  float h1 = step(mask, 0.99) * -0.2 - time * 0.05;
-  float h2 = step(mask, 0.5) * 0.1;
+  float h1 = step(mask, 0.99) * 0.4 - time * 0.05;
+  float h2 = step(mask, 0.4) * 0.5;
   vec3 hsv = vec3(h1 - h2, 0.45, 0.8);
   vec3 rgb = convertHsvToRgb(hsv);
   gl_FragColor = vec4(rgb, opacity);
