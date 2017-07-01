f455a380d2c8ef687777129f13d9113691ca8c29
yoichi kobayashi <info@tplh.net>
2017-07-01 23:52:15 +0900

    [add] range of noise and color to puddle objects,

--- src/glsl/sketch/puddle/puddle.fs
+++ src/glsl/sketch/puddle/puddle.fs
@@ -1,6 +1,8 @@
 precision highp float;
 
 uniform float time;
+uniform float colorH;
+uniform float noiseRange;
 
 varying vec2 vUv;
 
@@ -14,12 +16,12 @@ void main() {
   vec2 pos = vUv * 2.0 - vec2(1.0);
   float circleOut = smoothstep(0.0, 0.5, t1 - t2 - length(pos));
   float circleIn = smoothstep(0.0, 0.5, (t1 - t2) * 0.5 - length(pos));
-  float noise = (snoise3(vec3(pos * 1.6 + vec2(sin(time), cos(time)), time)) + 1.0) / 2.0;
+  float noise = (snoise3(vec3(pos * (1.4 + noiseRange * 0.1) + vec2(sin(time), cos(time)), time + noiseRange * 100.0)) + 1.0) / 2.0;
   float mask = circleOut * noise + circleIn;
   float opacity = 1.0 - step(mask, 0.3);
   float h1 = step(mask, 0.99) * 0.4 - time * 0.05;
   float h2 = step(mask, 0.4) * 0.5;
-  vec3 hsv = vec3(h1 - h2, 0.45, 0.8);
+  vec3 hsv = vec3(h1 - h2 + colorH, 0.45, 0.8);
   vec3 rgb = convertHsvToRgb(hsv);
   gl_FragColor = vec4(rgb, opacity);
 }
--- src/js/modules/sketch/puddle/Puddle.js
+++ src/js/modules/sketch/puddle/Puddle.js
@@ -6,7 +6,15 @@ export default class Puddle {
       time: {
         type: 'f',
         value: 0
-      }
+      },
+      colorH: {
+        type: 'f',
+        value: 0
+      },
+      noiseRange: {
+        type: 'f',
+        value: 0
+      },
     };
     this.obj = this.createObj();
     this.obj.visible = false;
@@ -24,16 +32,18 @@ export default class Puddle {
   }
   show(position) {
     this.uniforms.time.value = 0;
+    this.uniforms.colorH.value = Math.random();
+    this.uniforms.noiseRange.value = Math.random() * 3;
     this.obj.visible = true;
     this.obj.position.set(
-      (Math.random()* 2 - 1) * 1000,
-      (Math.random()* 2 - 1) * 1000,
+      (Math.random()* 2 - 1) * window.innerWidth,
+      (Math.random()* 2 - 1) * window.innerHeight,
       0.0
     );
   }
   render(time) {
     if (!this.obj.visible) return;
     this.uniforms.time.value += time;
-    this.obj.position.z -= 1;
+    this.obj.position.z -= 3;
   }
 }
