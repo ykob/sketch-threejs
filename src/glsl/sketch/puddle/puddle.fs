1ee29d40c5b8a60f057d4b4e7f386d38d6f57f16
yoichi kobayashi <info@tplh.net>
2017-07-01 12:16:31 +0900

    [add] colors to puddle objects.

--- src/glsl/sketch/puddle/puddle.fs
+++ src/glsl/sketch/puddle/puddle.fs
@@ -5,13 +5,19 @@ uniform float time;
 varying vec2 vUv;
 
 #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+#pragma glslify: convertHsvToRgb = require(glsl-util/convertHsvToRgb);
 
 void main() {
   float t = mod(time, 2.0) / 2.0;
   vec2 pos = vUv * 2.0 - vec2(1.0);
-  float circleOut = smoothstep(0.0, 0.2, 1.0 - length(pos));
-  float circleIn = smoothstep(0.0, 0.2, 0.8 - length(pos));
-  float noise = (snoise3(vec3(pos, time)) + 1.0) / 2.0;
-  float opacity = circleOut - noise + circleIn;
-  gl_FragColor = vec4(0.0, 0.0, 0.0, opacity);
+  float circleOut = smoothstep(0.0, 0.5, 1.0 - length(pos));
+  float circleIn = smoothstep(0.0, 0.5, 0.5 - length(pos));
+  float noise = (snoise3(vec3(pos * 1.6 + vec2(sin(time), cos(time)), time * 0.5)) + 1.0) / 2.0;
+  float mask = circleOut * noise + circleIn;
+  float opacity = 1.0 - step(mask, 0.3);
+  float h1 = step(mask, 0.99) * -0.2 - time * 0.05;
+  float h2 = step(mask, 0.5) * 0.1;
+  vec3 hsv = vec3(h1 - h2, 0.45, 0.8);
+  vec3 rgb = convertHsvToRgb(hsv);
+  gl_FragColor = vec4(rgb, opacity);
 }
