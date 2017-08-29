55c6b4eb4fca3a936d21e2dbe9e97bd93a499e75
yoichi kobayashi <info@tplh.net>
2017-08-30 01:48:43 +0900

    [modify] collect init functions into one object.

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,49 @@
-import initCommon from './init/common.js'
-import initCommonSketch from './init/commonSketch.js'
-import initIndex from './init/index.js'
-
-// initialize function for sketches
-import initPuddle from './init/puddle.js'
-import initReel from './init/reel.js'
-import initGlitch from './init/glitch.js'
-import initInstancing from './init/instancing.js'
-import initParticle from './init/particle.js'
-import initAttract from './init/attract.js'
-import initHole from './init/hole.js'
-import initMetalCube from './init/metalCube.js'
-import initDistort from './init/distort.js'
-import initImageData from './init/imageData.js'
-import initGallery from './init/gallery.js'
-import initComet from './init/comet.js'
-import initHyperSpace from './init/hyperSpace.js'
-import initFireBall from './init/fireBall.js'
-
 import ConsoleSignature from './modules/common/ConsoleSignature.js';
 import redirectOldSketches from './modules/common/redirectOldSketches.js';
 
+const init = {
+  common: require('./init/common.js').default,
+  index: require('./init/index.js').default,
+  sketch: {
+    common: require('./init/commonSketch.js').default,
+    puddle: require('./init/puddle.js').default,
+    reel: require('./init/reel.js').default,
+    glitch: require('./init/glitch.js').default,
+    instancing: require('./init/instancing.js').default,
+    particle: require('./init/particle.js').default,
+    attract: require('./init/attract.js').default,
+    hole: require('./init/hole.js').default,
+    metalCube: require('./init/metalCube.js').default,
+    distort: require('./init/distort.js').default,
+    imageData: require('./init/imageData.js').default,
+    gallery: require('./init/gallery.js').default,
+    comet: require('./init/comet.js').default,
+    hyperSpace: require('./init/hyperSpace.js').default,
+    fireBall: require('./init/fireBall.js').default,
+  }
+};
 const page = document.querySelector('.l-page');
 const pageId = page.dataset.id;
 const consoleSignature = new ConsoleSignature();
 
-const init = () => {
-  redirectOldSketches();
-  initCommon();
-  if (pageId !== 'index') initCommonSketch();
-  switch (pageId) {
-    case 'index': initIndex(); break;
-    case 'puddle': initPuddle(); break;
-    case 'reel': initReel(); break;
-    case 'glitch': initGlitch(); break;
-    case 'instancing': initInstancing(); break;
-    case 'particle': initParticle(); break;
-    case 'attract': initAttract(); break;
-    case 'hole': initHole(); break;
-    case 'metal_cube': initMetalCube(); break;
-    case 'distort': initDistort(); break;
-    case 'image_data': initImageData(); break;
-    case 'gallery': initGallery(); break;
-    case 'comet': initComet(); break;
-    case 'hyper_space': initHyperSpace(); break;
-    case 'fire_ball': initFireBall(); break;
-    default:
-  }
+redirectOldSketches();
+init.common();
+if (pageId !== 'index') init.sketch.common();
+switch (pageId) {
+  case 'index': init.index(); break;
+  case 'puddle': init.sketch.puddle(); break;
+  case 'reel': init.sketch.reel(); break;
+  case 'glitch': init.sketch.glitch(); break;
+  case 'instancing': init.sketch.instancing(); break;
+  case 'particle': init.sketch.particle(); break;
+  case 'attract': init.sketch.attract(); break;
+  case 'hole': init.sketch.hole(); break;
+  case 'metal_cube': init.sketch.metalCube(); break;
+  case 'distort': init.sketch.distort(); break;
+  case 'image_data': init.sketch.imageData(); break;
+  case 'gallery': init.sketch.gallery(); break;
+  case 'comet': init.sketch.comet(); break;
+  case 'hyper_space': init.sketch.hyperSpace(); break;
+  case 'fire_ball': init.sketch.fireBall(); break;
+  default:
 }
-init();
