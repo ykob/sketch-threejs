3d23f237ca584b381595e539a7a4cafafbbc6507
yoichi kobayashi <info@tplh.net>
2017-09-06 15:25:17 +0900

    [add] variation to butterfly color.

--- src/glsl/sketch/butterfly/butterfly.fs
+++ src/glsl/sketch/butterfly/butterfly.fs
@@ -1,5 +1,6 @@
 precision highp float;
 
+uniform float index;
 uniform float time;
 uniform float size;
 uniform sampler2D texture;
@@ -14,7 +15,7 @@ void main() {
   vec4 texColor = texture2D(texture, vUv);
 
   float noise = snoise3(vPosition / vec3(size * 0.25) + vec3(0.0, 0.0, time));
-  vec3 hsv = vec3(1.0 + noise * 0.3, 0.4, 1.0);
+  vec3 hsv = vec3(1.0 + noise * 0.2 + index * 0.7, 0.4, 1.0);
   vec3 rgb = convertHsvToRgb(hsv);
 
   gl_FragColor = vec4(rgb, 1.0) * texColor;
