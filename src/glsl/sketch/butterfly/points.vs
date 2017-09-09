f10d6d99352ba477ba2ae8e1c672811d6fad7262
yoichi kobayashi <info@tplh.net>
2017-09-10 04:16:43 +0900

    [modify] interval animation of points.

--- src/glsl/sketch/butterfly/points.vs
+++ src/glsl/sketch/butterfly/points.vs
@@ -1,20 +1,26 @@
 attribute vec3 position;
 attribute vec3 color;
-attribute float time;
+attribute float i;
+attribute float valid;
 
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 uniform float interval;
+uniform float time;
 
 varying vec3 vColor;
 varying float vOpacity;
 
 void main() {
-  vec3 updatePosition = position + vec3(0.0, pow(time, 2.0) * -10.0, 0.0);
+  float thisTime = mod(time + i / 200.0 * interval, interval);
+
+  vec3 updatePosition = position + vec3(0.0, pow(thisTime, 2.0) * -6.0, 0.0);
 
   vColor = color;
-  vOpacity = 1.0 - time / interval;
+  vOpacity = smoothstep(0.0, interval * 0.1, thisTime)
+    * (1.0 - smoothstep(interval * 0.66, interval * 1.0, thisTime))
+    * valid;
 
   gl_PointSize = 8.0;
   gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(updatePosition, 1.0);
--- src/js/modules/sketch/butterfly/Points.js
+++ src/js/modules/sketch/butterfly/Points.js
@@ -1,26 +1,40 @@
 const glslify = require('glslify');
-const SIZE = 100;
+const SIZE = 400;
+const INTERVAL = 3;
 
 export default class Points {
   constructor() {
     this.attr = {
       position: new THREE.BufferAttribute(new Float32Array(SIZE * 3), 3),
       color: new THREE.BufferAttribute(new Float32Array(SIZE * 3), 3),
-      time: new THREE.BufferAttribute(new Float32Array(SIZE), 1),
+      index: new THREE.BufferAttribute(new Float32Array(SIZE), 1),
+      valid: new THREE.BufferAttribute(new Float32Array(SIZE), 1),
     };
     this.geometry = new THREE.BufferGeometry();
     this.uniforms = {
       interval: {
         type: 'f',
-        value: 3
+        value: INTERVAL
+      },
+      time: {
+        type: 'f',
+        value: 0
       },
     };
+    this.index = 0;
     this.obj = this.createObj();
+    this.obj.renderOrder = 5;
   }
   createObj() {
+    const indices = [];
+    for (var i = 0; i < SIZE; i++) {
+      this.attr.index.setX(i, i);
+    }
     this.geometry.addAttribute('position', this.attr.position);
     this.geometry.addAttribute('color', this.attr.color);
-    this.geometry.addAttribute('time', this.attr.time);
+    this.geometry.addAttribute('i', this.attr.index);
+    this.geometry.addAttribute('valid', this.attr.valid);
+
     return new THREE.Points(
       this.geometry,
       new THREE.RawShaderMaterial({
@@ -33,22 +47,25 @@ export default class Points {
     );
   }
   render(time) {
+    this.uniforms.time.value += time;
     for (var i = 0; i < SIZE; i++) {
-      const now = this.attr.time.getX(i);
-      this.attr.time.setX(i, now + time);
-      if (now >= this.uniforms.interval.value) {
+      const time = (this.uniforms.time.value + this.attr.index.getX(i) / SIZE * INTERVAL) % INTERVAL;
+      const isValid = this.attr.valid.getX(i);
+
+      if (time >= INTERVAL && isValid == 1) {
+        this.attr.valid.setX(i, 0);
+      } else if (time <= INTERVAL && isValid == 0) {
         const radian = Math.random() * 360 * Math.PI / 180;
         const radius = Math.random() * 50 + 50;
-        this.attr.time.setX(i, 0);
         this.attr.position.setXYZ(
           i,
           Math.cos(radian) * radius,
-          Math.sin(radian) * radius,
+          Math.sin(radian) * radius * 0.5 + 100,
           0
         );
+        this.attr.valid.setX(i, 1);
       }
     }
     this.attr.position.needsUpdate = true;
-    this.attr.time.needsUpdate = true;
   }
 }
