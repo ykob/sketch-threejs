9235d2932042435cb855f13c1e0e35500961973d
yoichi kobayashi <info@tplh.net>
2017-09-16 01:20:08 +0900

    [modify] update opacity value of floor object.

--- src/glsl/sketch/transform/floor.fs
+++ src/glsl/sketch/transform/floor.fs
@@ -1,13 +1,19 @@
 precision highp float;
 
+uniform vec3 cameraPosition;
 uniform sampler2D texture;
 
+varying vec3 vPosition;
 varying vec4 vUv;
 
 void main() {
+  float opacity = clamp(1.0 - length(vPosition - cameraPosition) / 1000.0, 0.0, 1.0) * 0.3;
+  vec4 mirrorColor = vec4(vec3(1.0), opacity);
+
   vec4 projectorColor;
   if (all(bvec4(vUv.x >= 0.0, vUv.y >= 0.0, vUv.x <= vUv.z, vUv.y <= vUv.z))) {
     projectorColor = texture2DProj(texture, vUv);
   }
-  gl_FragColor = vec4(vec3(1.0), 0.3) * projectorColor;
+
+  gl_FragColor = mirrorColor * projectorColor;
 }
--- src/glsl/sketch/transform/floor.vs
+++ src/glsl/sketch/transform/floor.vs
@@ -6,10 +6,14 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 uniform mat4 textureMatrix;
 
+varying vec3 vPosition;
 varying vec4 vUv;
 
 void main(void) {
   vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+
+  vPosition = position;
   vUv = textureMatrix * worldPosition;
+
   gl_Position = projectionMatrix * viewMatrix * worldPosition;
 }
