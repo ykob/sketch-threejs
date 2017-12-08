59371a3a65044385dd75f3121ee9e00e6e53116f
yoichi kobayashi <info@tplh.net>
2017-12-09 02:37:48 +0900

    added process of flat shading to basic mesh fragment shader.

--- src/glsl/_base/mesh.fs
+++ src/glsl/_base/mesh.fs
@@ -1,8 +1,14 @@
 precision highp float;
 
-varying vec3 vColor;
+varying vec3 vPosition;
 varying vec2 vUv;
 
+const vec3 light = vec3(-0.7, 0.7, -0.7); // For Flat Shading
+
 void main() {
+  // Flat Shading
+  vec3 normal = normalize(cross(dFdx(vPosition), dFdy(vPosition)));
+  float diff = (dot(normal, light) + 1.0) / 2.0;
+
   gl_FragColor = vec4(1.0);
 }
