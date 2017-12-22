46be3b61d953ccca5c64cb36abdfabe4b395b7c2
yoichi kobayashi <info@tplh.net>
2017-12-22 18:11:19 +0900

    added original wrapper classes for loading some resources.

---
+++ src/js/modules/common/loadTexs.js
@@ -0,0 +1,23 @@
+const THREE = require('three/build/three.js');
+
+const texLoader = new THREE.TextureLoader();
+
+export default function(imgs, callback) {
+  const length = Object.keys(imgs).length;
+  const loadedTexs = {};
+  let count = 0;
+
+  for (var key in imgs) {
+    const k = key;
+    if (imgs.hasOwnProperty(k)) {
+      texLoader.load(imgs[k], (tex) => {
+        tex.magFilter = THREE.NearestFilter;
+        tex.minFilter = THREE.NearestFilter;
+        tex.repeat = THREE.RepeatWrapping;
+        loadedTexs[k] = tex;
+        count++;
+        if (count >= length) callback(loadedTexs);
+      });
+    }
+  }
+}
