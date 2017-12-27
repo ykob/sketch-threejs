624c792c38e37c8c6fd6ab608b4a6368be34e26b
yoichi kobayashi <info@tplh.net>
2017-12-27 17:04:36 +0900

    fixed error about const on base fragment shader.

--- src/glsl/_base/mesh.fs
+++ src/glsl/_base/mesh.fs
@@ -3,10 +3,9 @@ precision highp float;
 varying vec3 vPosition;
 varying vec2 vUv;
 
-const vec3 light = normalize(vec3(-1.0, 1.0, -1.0)); // For Flat Shading
-
 void main() {
   // Flat Shading
+  vec3 light = normalize(vec3(-1.0, 1.0, -1.0));
   vec3 normal = normalize(cross(dFdx(vPosition), dFdy(vPosition)));
   float diff = (dot(normal, light) + 1.0) / 2.0;
 
