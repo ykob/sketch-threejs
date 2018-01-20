c961f45a3b196bd531233cf5095e8b33c0d79a7f
yoichi kobayashi <info@tplh.net>
2018-01-20 12:06:46 +0900

    added base files for new sketch.

---
+++ src/js/init/recede.js
@@ -0,0 +1,66 @@
+const THREE = require('three/build/three.js');
+const debounce = require('js-util/debounce');
+
+export default function() {
+  // ==========
+  // Define common variables
+  //
+  const resolution = new THREE.Vector2();
+  const canvas = document.getElementById('canvas-webgl');
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+    canvas: canvas,
+  });
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera();
+  const clock = new THREE.Clock();
+
+  camera.far = 50000;
+  camera.setFocalLength(24);
+
+  // ==========
+  // Define unique variables
+  //
+
+  // ==========
+  // Define functions
+  //
+  const render = () => {
+    const time = clock.getDelta();
+    renderer.render(scene, camera);
+  };
+  const renderLoop = () => {
+    render();
+    requestAnimationFrame(renderLoop);
+  };
+  const resizeCamera = () => {
+    camera.aspect = resolution.x / resolution.y;
+    camera.updateProjectionMatrix();
+  };
+  const resizeWindow = () => {
+    resolution.set(document.body.clientWidth, window.innerHeight);
+    canvas.width = resolution.x;
+    canvas.height = resolution.y;
+    resizeCamera();
+    renderer.setSize(resolution.x, resolution.y);
+  };
+  const on = () => {
+    window.addEventListener('resize', debounce(resizeWindow), 1000);
+  };
+
+  // ==========
+  // Initialize
+  //
+  const init = () => {
+    renderer.setClearColor(0xeeeeee, 1.0);
+    camera.position.set(1000, 1000, 1000);
+    camera.lookAt(new THREE.Vector3());
+    clock.start();
+
+    on();
+    resizeWindow();
+    renderLoop();
+  }
+  init();
+}
