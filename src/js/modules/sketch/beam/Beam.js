7eeea2e769099fc3addadb18aed08d9cc2d49750
yoichi kobayashi <info@tplh.net>
2018-06-23 16:37:53 +0900

    clean up.

--- src/js/modules/sketch/beam/Beam.js
+++ src/js/modules/sketch/beam/Beam.js
@@ -32,7 +32,7 @@ export default class Beam {
         0,
         ((Math.random() + Math.random() + Math.random()) / 3 * 2 - 1) * 150,
       );
-      rotate.setXYZ(i, (MathEx.randomInt(0, 1) * 2 - 1) * 90 + (Math.random(0, 1) * 2 - 1) * 60);
+      rotate.setXYZ(i, (MathEx.randomInt(0, 1) * 2 - 1) * 90 + (Math.random() * 2 - 1) * 60);
       delay.setXYZ(i, Math.random() * 2.0);
     }
     geometry.addAttribute('instancePosition', instancePosition);
