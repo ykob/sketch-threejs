4221727ad16a16d57c89793adb6eb091dfacbc63
yoichi kobayashi <info@tplh.net>
2018-08-01 01:55:00 +0900

    update how to generate landscape object.

--- src/js/modules/sketch/land/Land.js
+++ src/js/modules/sketch/land/Land.js
@@ -17,7 +17,7 @@ export default class Mesh {
     // Define Geometry
     const baseY = 200;
     const simplex = new SimplexNoise(Math.random);
-    const geometry = new THREE.BoxBufferGeometry(2000, baseY, 2000, 50, 1, 50);
+    const geometry = new THREE.BoxBufferGeometry(2000, baseY, 2000, 100, 1, 100);
     for (var i = 0; i < geometry.attributes.position.count; i++) {
       const x = geometry.attributes.position.getX(i);
       const y = geometry.attributes.position.getY(i);
@@ -40,15 +40,29 @@ export default class Mesh {
         z / 100,
         1
       );
-      const updateY = Math.max(
-        0,
-        noise1 * 800
-        + noise2 * 200
-        + noise3 / 2 * 20
+      const noise4 = simplex.noise4D(
+        x / 2400,
+        y / 600,
+        z / 600,
+        1
       );
-      const isEnabled = (y / baseY + 0.5);
+      const step = (e, x) => {
+        return (x >= e) ? 1 : 0;
+      };
+      const smoothstep = (e0, e1, x) => {
+        if (e0 >= e1) return undefined;
+        var t = MathEx.clamp((x - e0) / (e1 - e0), 0, 1);
+        return t * t * (3 - 2 * t);
+      };
+      const updateY =
+        (noise1 * 0.75 + 0.25) * 800
+        + noise2 * 120
+        + noise3 * 20
+        + noise4 * 200;
+      const s = smoothstep(0, 100, updateY);
+      const isBottom = step(0, y);
 
-      geometry.attributes.position.setY(i, (y + updateY) * isEnabled);
+      geometry.attributes.position.setY(i, (y + updateY * s) * isBottom);
     }
 
     // Define Material
