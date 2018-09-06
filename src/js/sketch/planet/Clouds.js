c0904e436ba6b75a888afab0095d1da7c47344f2
yoichi kobayashi <info@tplh.net>
2018-09-06 10:48:53 +0900

    rotate clouds instance object.

--- src/js/sketch/planet/Clouds.js
+++ src/js/sketch/planet/Clouds.js
@@ -10,23 +10,33 @@ export default class Clouds {
         value: 0
       },
     };
-    this.num = 100;
+    this.num = 50;
     this.obj = null;
   }
   createObj() {
     // Define Geometries
     const geometry = new THREE.InstancedBufferGeometry();
-    const baseGeometry = new THREE.BoxBufferGeometry(10, 10, 10);
+    const baseGeometry = new THREE.BoxBufferGeometry(4, 4, 12);
 
     // Copy attributes of the base Geometry to the instancing Geometry
     geometry.copy(baseGeometry);
 
     // Define attributes of the instancing geometry
     const instancePositions = new THREE.InstancedBufferAttribute(new Float32Array(this.num * 3), 3, 1);
+    const instanceRotates = new THREE.InstancedBufferAttribute(new Float32Array(this.num * 3), 3, 1);
     for ( var i = 0, ul = this.num; i < ul; i++ ) {
-      instancePositions.setXYZ(i, 0, 60, 0);
+      const rx = MathEx.radians(((Math.random() * 2) - 1) * 45);
+      const ry = MathEx.radians(((Math.random() * 2) - 1) * 180);
+      const p = MathEx.spherical(
+        rx,
+        ry,
+        Math.random() * 10 + 70
+      );
+      instancePositions.setXYZ(i, p[0], p[1], p[2]);
+      instanceRotates.setXYZ(i, 0, ry, -rx);
     }
     geometry.addAttribute('instancePosition', instancePositions);
+    geometry.addAttribute('instanceRotate', instanceRotates);
 
     // Define Material
     const material = new THREE.RawShaderMaterial({
--- src/js/sketch/planet/glsl/clouds.vs
+++ src/js/sketch/planet/glsl/clouds.vs
@@ -1,5 +1,6 @@
 attribute vec3 position;
 attribute vec3 instancePosition;
+attribute vec3 instanceRotate;
 attribute vec2 uv;
 
 uniform mat4 projectionMatrix;
@@ -9,11 +10,13 @@ varying vec3 vPosition;
 varying vec2 vUv;
 
 #pragma glslify: calcTranslateMat4 = require(glsl-matrix/calcTranslateMat4);
+#pragma glslify: calcRotateMat4 = require(glsl-matrix/calcRotateMat4);
 
 void main(void) {
   // coordinate transformation
   mat4 translateMat = calcTranslateMat4(instancePosition);
-  vec4 mvPosition = modelViewMatrix * translateMat * vec4(position, 1.0);
+  mat4 rotateMat = calcRotateMat4(instanceRotate);
+  vec4 mvPosition = modelViewMatrix * translateMat * rotateMat * vec4(position, 1.0);
 
   vPosition = position;
   vUv = uv;
