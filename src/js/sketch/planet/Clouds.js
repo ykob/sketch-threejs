d4564e46655bdbcf8fde86f5f9bea2b07f32e578
yoichi kobayashi <info@tplh.net>
2018-09-06 15:18:09 +0900

    added noise and animation to clouds.

--- src/js/sketch/planet/Clouds.js
+++ src/js/sketch/planet/Clouds.js
@@ -16,7 +16,10 @@ export default class Clouds {
   createObj() {
     // Define Geometries
     const geometry = new THREE.InstancedBufferGeometry();
-    const baseGeometry = new THREE.BoxBufferGeometry(4, 4, 12);
+    // const baseGeometry = new THREE.SphereBufferGeometry(2, 6, 18);
+    const baseGeometry = new THREE.BoxBufferGeometry(2, 2, 20, 2, 2, 6);
+    // baseGeometry.scale(1, 5, 1);
+    // baseGeometry.rotateX(MathEx.radians(90));
 
     // Copy attributes of the base Geometry to the instancing Geometry
     geometry.copy(baseGeometry);
@@ -24,19 +27,22 @@ export default class Clouds {
     // Define attributes of the instancing geometry
     const instancePositions = new THREE.InstancedBufferAttribute(new Float32Array(this.num * 3), 3, 1);
     const instanceRotates = new THREE.InstancedBufferAttribute(new Float32Array(this.num * 3), 3, 1);
+    const speeds = new THREE.InstancedBufferAttribute(new Float32Array(this.num), 1, 1);
     for ( var i = 0, ul = this.num; i < ul; i++ ) {
-      const rx = MathEx.radians(((Math.random() * 2) - 1) * 45);
+      const rx = MathEx.radians(((Math.random() * 2) - 1) * 30);
       const ry = MathEx.radians(((Math.random() * 2) - 1) * 180);
       const p = MathEx.spherical(
         rx,
         ry,
-        Math.random() * 10 + 70
+        Math.random() * 20 + 70
       );
       instancePositions.setXYZ(i, p[0], p[1], p[2]);
       instanceRotates.setXYZ(i, 0, ry, -rx);
+      speeds.setXYZ(i, Math.random() * 0.05 + 0.01);
     }
     geometry.addAttribute('instancePosition', instancePositions);
     geometry.addAttribute('instanceRotate', instanceRotates);
+    geometry.addAttribute('speed', speeds);
 
     // Define Material
     const material = new THREE.RawShaderMaterial({
--- src/js/sketch/planet/glsl/clouds.vs
+++ src/js/sketch/planet/glsl/clouds.vs
@@ -2,21 +2,31 @@ attribute vec3 position;
 attribute vec3 instancePosition;
 attribute vec3 instanceRotate;
 attribute vec2 uv;
+attribute float speed;
 
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
+uniform float time;
 
 varying vec3 vPosition;
 varying vec2 vUv;
 
 #pragma glslify: calcTranslateMat4 = require(glsl-matrix/calcTranslateMat4);
 #pragma glslify: calcRotateMat4 = require(glsl-matrix/calcRotateMat4);
+#pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+#pragma glslify: ease = require(glsl-easings/circular-out);
 
 void main(void) {
+  // added Noise to form like a cloud.
+  float noise = snoise3(position + instancePosition);
+  vec3 noiseEasePos = normalize(vec3(position.x, position.y, 0.01)) * ease((10.0 - abs(position.z)) / 10.0) * vec3(0.4, 1.0, 1.0);
+  vec3 noisePosition = (noise + 1.0) / 2.0 * noiseEasePos * 3.0 + noiseEasePos * 1.0;
+
   // coordinate transformation
   mat4 translateMat = calcTranslateMat4(instancePosition);
   mat4 rotateMat = calcRotateMat4(instanceRotate);
-  vec4 mvPosition = modelViewMatrix * translateMat * rotateMat * vec4(position, 1.0);
+  mat4 worldRotateMat = calcRotateMat4(vec3(0.0, time * speed, 0.0));
+  vec4 mvPosition = modelViewMatrix * worldRotateMat * translateMat * rotateMat * vec4(position + noisePosition, 1.0);
 
   vPosition = mvPosition.xyz;
   vUv = uv;
