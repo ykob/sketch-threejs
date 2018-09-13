eac2ed0916af123f22fc77bce37357c2bebbf8d1
yoichi kobayashi <info@tplh.net>
2018-09-13 17:51:45 +0900

    removed gulp-babel

--- gulp/tasks/uglify.js
+++ gulp/tasks/uglify.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
 const pump = require('pump');
 
 const $ = require('../plugins');
@@ -9,16 +8,6 @@ gulp.task('uglify', (cb) => {
   pump(
     [
       gulp.src(conf.src),
-      $.babel({
-        babelrc: false,
-        presets: [
-          ['env', {
-            targets: {
-              browsers: ['last 2 versions', 'ie >= 11']
-            }
-          }]
-        ]
-      }),
       $.uglify(conf.opts),
       $.rename({
         suffix: '.min'
