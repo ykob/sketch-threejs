cd8ade596b515a8772aa60f6dcc125fd3df1b308
yoichi kobayashi <info@tplh.net>
2018-10-06 22:49:22 +0900

    updated the wave object colors in sketch buddha.

--- src/js/sketch/buddha/glsl/wave.fs
+++ src/js/sketch/buddha/glsl/wave.fs
@@ -2,12 +2,20 @@ precision highp float;
 
 varying vec3 vPosition;
 varying vec2 vUv;
+varying float vOpacity;
+
+#pragma glslify: convertHsvToRgb = require(glsl-util/convertHsvToRgb);
 
 void main() {
   // Flat Shading
-  vec3 light = normalize(vec3(-0.5, 1.0, -1.0));
+  vec3 light = normalize(vec3(0.5, 0.5, 1.0));
   vec3 normal = normalize(cross(dFdx(vPosition), dFdy(vPosition)));
   float diff = (dot(normal, light) + 1.0) / 2.0;
+  float glow = smoothstep(0.9, 1.0, diff);
+  float shadow = diff;
 
-  gl_FragColor = vec4(vec3(0.0) + diff, 1.0);
+  vec3 hsv = vec3(0.1, 0.0, 0.05 + (glow * 6.0) / 10.0);
+  vec3 rgb = convertHsvToRgb(hsv);
+
+  gl_FragColor = vec4(rgb, vOpacity);
 }
