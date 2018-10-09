c13123e3d4c74e7fdb9ac11a5f602515b581a111
yoichi kobayashi <info@tplh.net>
2018-10-09 18:02:16 +0900

    change var to const.

--- src/js/sketch/buddha/Typo.js
+++ src/js/sketch/buddha/Typo.js
@@ -18,12 +18,12 @@ export default class InstanceMesh {
     this.obj = null;
   }
   createTexture() {
-    var text = '観自在菩薩行深般若波羅蜜多時照見五蘊皆空度一切苦厄舎利子色不異空空不異色色即是空空即是色受想行識亦復如是舎利子是諸法空相不生不滅不垢不浄不増不減是故空中無色無受想行識無眼耳鼻舌身意無色声香味触法無眼界乃至無意識界無無明亦無無明尽乃至無老死亦無老死尽無苦集滅道無智亦無得以無所得故菩提薩埵依般若波羅蜜多故心無罣礙無罣礙故無有恐怖遠離一切顛倒夢想究竟涅槃三世諸仏依般若波羅蜜多故得阿耨多羅三藐三菩提故知般若波羅蜜多是大神呪是大明呪是無上呪是無等等呪能除一切苦真実不虚故説般若波羅蜜多呪即説呪日羯諦羯諦波羅羯諦波羅僧羯諦菩提薩婆訶般若心経';
-    var widthPerSide = 2048;
-    var gridsPerSide = Math.ceil(Math.sqrt(text.length));
-    var fontSize = widthPerSide / gridsPerSide;
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d', {
+    const text = '観自在菩薩行深般若波羅蜜多時照見五蘊皆空度一切苦厄舎利子色不異空空不異色色即是空空即是色受想行識亦復如是舎利子是諸法空相不生不滅不垢不浄不増不減是故空中無色無受想行識無眼耳鼻舌身意無色声香味触法無眼界乃至無意識界無無明亦無無明尽乃至無老死亦無老死尽無苦集滅道無智亦無得以無所得故菩提薩埵依般若波羅蜜多故心無罣礙無罣礙故無有恐怖遠離一切顛倒夢想究竟涅槃三世諸仏依般若波羅蜜多故得阿耨多羅三藐三菩提故知般若波羅蜜多是大神呪是大明呪是無上呪是無等等呪能除一切苦真実不虚故説般若波羅蜜多呪即説呪日羯諦羯諦波羅羯諦波羅僧羯諦菩提薩婆訶般若心経';
+    const widthPerSide = 2048;
+    const gridsPerSide = Math.ceil(Math.sqrt(text.length));
+    const fontSize = widthPerSide / gridsPerSide;
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d', {
       alpha: true
     });
 
@@ -36,6 +36,8 @@ export default class InstanceMesh {
         ctx.fillText(str, fontSize * x, fontSize * (y + 1) - fontSize * 0.15);
       }
     }
+
+
   }
   createObj() {
     this.createTexture();
