a591d394daccddbcd6af5ba8c536817d4b64acdb
yoichi kobayashi <info@tplh.net>
2018-11-05 01:53:20 +0900

    renamed instacedBufferAttribute variable in default files.

--- src/js/sketch/_default/InstanceMesh.js
+++ src/js/sketch/_default/InstanceMesh.js
@@ -21,11 +21,11 @@ export default class InstanceMesh {
     geometry.copy(baseGeometry);
 
     // Define attributes of the instancing geometry
-    const iPositions = new THREE.InstancedBufferAttribute(new Float32Array(this.num * 3), 3);
+    const ibaPositions = new THREE.InstancedBufferAttribute(new Float32Array(this.num * 3), 3);
     for ( var i = 0, ul = this.num; i < ul; i++ ) {
       iPositions.setXYZ(i, 0, 0, 0);
     }
-    geometry.addAttribute('iPosition', iPositions);
+    geometry.addAttribute('iPosition', ibaPositions);
 
     // Define Material
     const material = new THREE.RawShaderMaterial({
