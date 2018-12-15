12e0df3e1ce12348e9b8c22a409497fb5bf5f850
yoichi kobayashi <info@tplh.net>
2018-12-16 01:10:32 +0900

    update the noise value in the disolve animation of the typo object.

--- src/js/sketch/newyear2019/glsl/typo.fs
+++ src/js/sketch/newyear2019/glsl/typo.fs
@@ -15,8 +15,8 @@ void main() {
   vec4 texColor = texture2D(tex, vUv);
 
   float noise = cnoise3(vPosition * vec3(1.6)) * 0.5 + 0.5;
-  float opacity1 = smoothstep(0.0, 0.01, (1.5 - length(p)) + noise + vTime1 * 2.5 - 2.5);
-  float opacity2 = smoothstep(0.0, 0.01, (1.5 - length(p)) + noise + vTime2 * 2.5 - 2.5);
+  float opacity1 = smoothstep(0.0, 0.01, (1.2 - length(p)) + noise * 0.8 + vTime1 * 2.0 - 2.0);
+  float opacity2 = smoothstep(0.0, 0.01, (1.2 - length(p)) + noise * 0.8 + vTime2 * 2.0 - 2.0);
   float opacity = opacity1 * 0.9 - opacity2 * 0.85;
 
   if (texColor.a < 0.5) discard;
