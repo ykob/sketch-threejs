4095f873510ffdae45f4c26b28b52ced76e2b748
yoichi kobayashi <info@tplh.net>
2018-12-24 18:54:47 +0900

    update gulp version v3 to v4.

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -6,11 +6,16 @@ const $ = require('../plugins');
 const DIR = require('../conf').DIR;
 const conf = require('../conf').scripts;
 
+const webpackError = function() {
+  this.emit('end');
+};
+
 gulp.task('scripts', () => {
   conf.webpack.mode = process.env.NODE_ENV;
   if (conf.webpack.mode == 'development') {
-    gulp.src(conf.src)
+    return gulp.src(conf.src)
       .pipe(webpackStream(conf.webpack, webpack))
+      .on('error', webpackError)
       .pipe(gulp.dest(conf.dest[conf.webpack.mode]));
   } else {
     return webpackStream(conf.webpack, webpack)
