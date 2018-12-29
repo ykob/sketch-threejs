5b3ad4d1cfdbf0327533b62ec4bd181559c406dd
yoichi kobayashi <info@tplh.net>
2018-12-29 19:10:34 +0900

    adjust typo object fragment.

--- src/js/sketch/newyear2019/glsl/typo.fs
+++ src/js/sketch/newyear2019/glsl/typo.fs
@@ -25,14 +25,14 @@ void main() {
 
   vec4 color1 = vec4(vec3(1.0, 1.0, 0.9), 0.9) * (opacity1 - opacity2);
   vec4 color2 = vec4(vec3(1.0, 1.0, 0.9), 0.05) * (opacity2 - opacity3);
-  vec4 color3 = vec4(vec3(0.7, 0.1, 0.1), 0.8) * (opacity3 - opacity4);
+  vec4 color3 = vec4(vec3(0.7, 0.1, 0.1), 0.9) * (opacity3 - opacity4);
   vec4 color4 = vec4(vec3(1.0, 1.0, 0.9), 0.05) * opacity4;
 
   if (texColor.a < 0.5) discard;
   gl_FragColor =
     (
       (color1 + color2 + color3 + color4) * (1.0 - drawBrightOnly)
-      + vec4(vec3(0.0), 1.0) * drawBrightOnly
+      + vec4(vec3(0.0), opacity3 - opacity4) * drawBrightOnly
     )
     * vec4(vec3(1.0), texColor.a);
 }
