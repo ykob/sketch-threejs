d3de2b132007451c1b6aacdc0928429a051fcf40
yoichi kobayashi <info@tplh.net>
2019-01-08 16:30:42 +0900

    added DNA Helix line.

--- src/js/sketch/dna/DnaHelix.js
+++ src/js/sketch/dna/DnaHelix.js
@@ -9,7 +9,6 @@ export default class DnaHelix {
         value: 0
       },
     };
-    this.num = 6000;
     this.obj;
   }
   createObj() {
@@ -17,11 +16,15 @@ export default class DnaHelix {
     const geometry = new THREE.BufferGeometry();
 
     // Define attributes of the instancing geometry
-    const baPositions = new THREE.BufferAttribute(new Float32Array(this.num * 3), 3);
-    const baRadians = new THREE.BufferAttribute(new Float32Array(this.num), 1);
-    const baRadiuses = new THREE.BufferAttribute(new Float32Array(this.num), 1);
-    const baDelays = new THREE.BufferAttribute(new Float32Array(this.num), 1);
-    for ( var i = 0, ul = this.num; i < ul; i++ ) {
+    const numHelix = 6000;
+    const numLineSpace = 60;
+    const numLine = 100;
+    const numAmount = numHelix + numLineSpace * numLine;
+    const baPositions = new THREE.BufferAttribute(new Float32Array(numAmount * 3), 3);
+    const baRadians = new THREE.BufferAttribute(new Float32Array(numAmount), 1);
+    const baRadiuses = new THREE.BufferAttribute(new Float32Array(numAmount), 1);
+    const baDelays = new THREE.BufferAttribute(new Float32Array(numAmount), 1);
+    for (var i = 0; i < numHelix; i++) {
       const random = Math.random();
       const diff = {
         x: (Math.random() * 2 - 1) * random * 6,
@@ -30,13 +33,34 @@ export default class DnaHelix {
       };
       baPositions.setXYZ(
         i,
-        ((i / this.num) * 2 - 1) * 150 + diff.x,
+        ((i / numHelix) * 2 - 1) * 150 + diff.x,
         diff.y,
         diff.z
       );
-      baRadians.setX(i, MathEx.radians(i / this.num * 900 + i % 2 * 180));
+      baRadians.setX(i, MathEx.radians(i / numHelix * 900 + i % 2 * 180));
       baRadiuses.setX(i, 18);
       baDelays.setX(i, MathEx.radians(Math.random() * 360));
+    }
+    for (var j = 0; j < numLineSpace; j++) {
+      const radians = MathEx.radians(j / numLineSpace * 900);
+      for (var k = 0; k < numLine; k++) {
+        const index = j * numLine + k + numHelix;
+        const random = Math.random();
+        const diff = {
+          x: (Math.random() * 2 - 1) * random * 1,
+          y: (Math.random() * 2 - 1) * random * 1,
+          z: (Math.random() * 2 - 1) * random * 1,
+        };
+        baPositions.setXYZ(
+          index,
+          ((j / numLineSpace) * 2 - 1) * 150 + diff.x,
+          diff.y,
+          diff.z
+        );
+        baRadians.setX(index, radians);
+        baRadiuses.setX(index, (k / numLine * 2 - 1) * 18);
+        baDelays.setX(index, MathEx.radians(Math.random() * 360));
+      }
     }
     geometry.addAttribute('position', baPositions);
     geometry.addAttribute('radian', baRadians);
