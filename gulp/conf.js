7b48abd6cc4022da4fb20a66c0cd4b64756c355e
yoichi kobayashi <info@tplh.net>
2019-04-08 14:55:56 +0900

    removed browsers property in gulp sass task.

--- gulp/conf.js
+++ gulp/conf.js
@@ -113,12 +113,6 @@ module.exports.sass = {
     `!${DIR.SRC}/**/_*.{sass,scss}`
   ],
   dest: `${DIR.DEST}/css`,
-  browsers: [
-    'last 2 versions',
-    'ie >= 9',
-    'Android >= 4',
-    'ios_saf >= 8',
-  ]
 };
 
 module.exports.replace = {
