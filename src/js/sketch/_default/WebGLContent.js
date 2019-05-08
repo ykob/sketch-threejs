5a5996047e71e20999f48ecd75e8e67fb061b3b0
yoichi kobayashi <info@tplh.net>
2019-05-08 16:42:29 +0900

    update the WebGL basis structure of default files.

---
+++ src/js/sketch/_default/WebGLContent.js
@@ -0,0 +1,87 @@
+import * as THREE from 'three';
+import sleep from 'js-util/sleep';
+
+const canvas = document.getElementById('canvas-webgl');
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+  canvas: canvas,
+});
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera();
+const cameraResolution = new THREE.Vector2();
+const clock = new THREE.Clock({
+  autoStart: false
+});
+
+const resizeCamera = (resolution) => {
+  if (resolution.x > resolution.y) {
+    cameraResolution.set(
+      (resolution.x >= 1200) ? 1200 : resolution.x,
+      (resolution.x >= 1200) ? 800 : resolution.x * 0.66,
+    );
+  } else {
+    cameraResolution.set(
+      ((resolution.y >= 1200) ? 800 : resolution.y * 0.66) * 0.6,
+      ((resolution.y >= 1200) ? 1200 : resolution.y) * 0.6,
+    );
+  }
+  camera.setViewOffset(
+    cameraResolution.x,
+    cameraResolution.y,
+    (resolution.x - cameraResolution.x) / -2,
+    (resolution.y - cameraResolution.y) / -2,
+    resolution.x,
+    resolution.y
+  );
+  camera.updateProjectionMatrix();
+};
+
+extend default class WebGLContent {
+  constructor() {
+  }
+  async init() {
+    renderer.setClearColor(0xeeeeee, 1.0);
+
+    camera.aspect = 3 / 2;
+    camera.far = 1000;
+    camera.setFocalLength(50);
+    camera.position.set(0, 0, 50);
+    camera.lookAt(new THREE.Vector3());
+  }
+  start() {
+    this.play();
+  }
+  stop() {
+    this.pause();
+  }
+  play() {
+    clock.start();
+    this.update();
+  }
+  pause() {
+    clock.stop();
+  }
+  update() {
+    const time = clock.getDelta();
+
+    // Update each objects.
+
+    // Render the 3D scene.
+    renderer.render(scene, camera);
+
+    // When the clock is stopped, it stops the all rendering too.
+    if (clock.running === false) return;
+
+    // Iterate the rendering.
+    requestAnimationFrame(() => {
+      this.update();
+    });
+  }
+  resize(resolution) {
+    canvas.width = resolution.x;
+    canvas.height = resolution.y;
+    resizeCamera(resolution);
+    renderer.setSize(resolution.x, resolution.y);
+  }
+}
