8a12eb78c5d53ded9d00b1e3b0502214b673d622
yoichi kobayashi <info@tplh.net>
2019-05-20 13:20:48 +0900

    update the WebGL basis structure of default files.

--- src/js/sketch/_default/init.js
+++ src/js/sketch/_default/init.js
@@ -1,101 +1,37 @@
 import * as THREE from 'three';
 import debounce from 'js-util/debounce';
-import sleep from 'js-util/sleep';
+
+import WebGLContent from './WebGLContent';
 
 export default async function() {
-  // ==========
-  // Define common variables
-  //
+  const webglContent = new WebGLContent();
   const resolution = new THREE.Vector2();
-  const canvas = document.getElementById('canvas-webgl');
-  const renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    antialias: true,
-    canvas: canvas,
-  });
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera();
-  const cameraResolution = new THREE.Vector2();
-  const clock = new THREE.Clock({
-    autoStart: false
-  });
-
-  // For the preloader.
   const preloader = document.querySelector('.p-preloader');
 
-  // ==========
-  // Define unique variables
-  //
-
-  // ==========
-  // Define functions
-  //
-  const render = () => {
-    const time = clock.getDelta();
-    renderer.render(scene, camera);
-  };
-  const renderLoop = () => {
-    render();
-    requestAnimationFrame(renderLoop);
-  };
-  const resizeCamera = () => {
-    if (resolution.x > resolution.y) {
-      cameraResolution.set(
-        (resolution.x >= 1200) ? 1200 : resolution.x,
-        (resolution.x >= 1200) ? 800 : resolution.x * 0.66,
-      );
-    } else {
-      cameraResolution.set(
-        ((resolution.y >= 1200) ? 800 : resolution.y * 0.66) * 0.6,
-        ((resolution.y >= 1200) ? 1200 : resolution.y) * 0.6,
-      );
-    }
-    camera.setViewOffset(
-      cameraResolution.x,
-      cameraResolution.y,
-      (resolution.x - cameraResolution.x) / -2,
-      (resolution.y - cameraResolution.y) / -2,
-      resolution.x,
-      resolution.y
-    );
-    camera.updateProjectionMatrix();
-  };
   const resizeWindow = () => {
     resolution.set(document.body.clientWidth, window.innerHeight);
-    canvas.width = resolution.x;
-    canvas.height = resolution.y;
-    resizeCamera();
-    renderer.setSize(resolution.x, resolution.y);
+    webglContent.resize(resolution);
   };
   const on = () => {
     window.addEventListener('blur', () => {
-      // this window is inactive.
-      clock.stop();
+      webglContent.pause();
     });
     window.addEventListener('focus', () => {
-      // this window is inactive.
-      clock.start();
+      webglContent.play();
     });
     window.addEventListener('resize', debounce(resizeWindow, 100));
   };
-
-  // ==========
-  // Initialize
-  //
-  renderer.setClearColor(0xeeeeee, 1.0);
-
-  camera.aspect = 3 / 2;
-  camera.far = 1000;
-  camera.setFocalLength(50);
-  camera.position.set(0, 0, 50);
-  camera.lookAt(new THREE.Vector3());
+  const render = () => {
+    webglContent.update();
+    requestAnimationFrame(render);
+  };
 
   on();
   resizeWindow();
 
-  // For the preloader.
-  const preloader = document.querySelector('.p-preloader');
+  await webglContent.init();
 
-  clock.start();
-  renderLoop();
+  preloader.classList.add('is-hidden');
+  webglContent.start();
+  render();
 }
