e0a5d40ac57081bc8296920acc04d8a0d493c933
yoichi kobayashi <info@tplh.net>
2019-08-14 12:08:13 +0900

    added the pug file and related files of the new sketch "burn".

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,7 @@ if (pageId == 'index') {
   });
 
   switch (pageId) {
+    case 'burn':        require('./sketch/burn/init.js').default(); break;
     case 'crystal':     require('./sketch/crystal/init.js').default(); break;
     case 'node':        require('./sketch/node/init.js').default(); break;
     case 'sun':         require('./sketch/sun/init.js').default(); break;
