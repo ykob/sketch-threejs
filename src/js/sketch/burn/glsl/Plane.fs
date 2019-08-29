241eddb44774d204217720835f4fb889f0b9a03f
yoichi kobayashi <info@tplh.net>
2019-08-29 19:33:00 +0900

    added to calculate the UV for the noise texture.

--- src/js/sketch/burn/glsl/Plane.fs
+++ src/js/sketch/burn/glsl/Plane.fs
@@ -1,12 +1,19 @@
 precision highp float;
 
+uniform vec2 imgRatio;
 uniform sampler2D texNoise;
 
 varying vec3 vPosition;
 varying vec2 vUv;
 
 void main() {
-  vec4 noise = texture2D(texNoise, vUv);
+  vec2 updateUv = vUv * imgRatio + vec2(
+    (1.0 - imgRatio.x) * 0.5,
+    (1.0 - imgRatio.y) * 0.5
+    );
 
-  gl_FragColor = noise;
+  vec4 noiseRG = texture2D(texNoise, updateUv);
+  vec4 noiseB = texture2D(texNoise, vUv);
+
+  gl_FragColor = vec4(noiseRG.rg, noiseB.b, 1.0);
 }
