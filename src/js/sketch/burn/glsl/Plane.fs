24148691c9225f074d9495bcc4958069c31c6ca2
yoichi kobayashi <info@tplh.net>
2019-09-03 09:46:03 +0900

    adjusted the noise mask color for the burn plane.

--- src/js/sketch/burn/glsl/Plane.fs
+++ src/js/sketch/burn/glsl/Plane.fs
@@ -20,10 +20,12 @@ void main() {
   float noiseG = texture2D(texNoise, updateUv + vec2(time * 0.2, 0.0)).g;
   float slide = texture2D(texNoise, vUv).b;
 
-  float maskSlide = smoothstep(0.0, 0.2, t * 1.2 - (slide * 0.6 + noiseR * 0.2 + noiseG * 0.2));
+  float mask = t * 1.205 - (slide * 0.6 + noiseR * 0.2 + noiseG * 0.2);
+  float maskPrev = 1.0 - smoothstep(0.0, 0.1, mask);
+  float maskNext = smoothstep(0.105, 0.205, mask);
 
-  vec3 color1 = vec3(1.0, 0.0, 0.0) * (1.0 - maskSlide);
-  vec3 color2 = vec3(0.0, 1.0, 0.0) * maskSlide;
+  vec3 color1 = vec3(1.0, 0.0, 0.0) * maskPrev;
+  vec3 color2 = vec3(0.0, 1.0, 0.0) * maskNext;
 
   gl_FragColor = vec4(color1 + color2, 1.0);
 }
