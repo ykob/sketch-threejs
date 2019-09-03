161380e0ccd634de1a1d3a0fc6c2dea43337b7f9
yoichi kobayashi <info@tplh.net>
2019-09-03 19:29:15 +0900

    update the noise mask for the burn plane.

--- src/js/sketch/burn/glsl/Plane.fs
+++ src/js/sketch/burn/glsl/Plane.fs
@@ -18,11 +18,11 @@ void main() {
 
   float noiseR = texture2D(texNoise, updateUv + vec2(time * 0.1, 0.0)).r;
   float noiseG = texture2D(texNoise, updateUv + vec2(time * 0.2, 0.0)).g;
-  float slide = texture2D(texNoise, vUv).b;
+  float slide = texture2D(texNoise, vUv * vec2(0.998) + 0.001).b;
 
-  float mask = t * 1.205 - (slide * 0.6 + noiseR * 0.2 + noiseG * 0.2);
-  float maskPrev = 1.0 - smoothstep(0.0, 0.1, mask);
-  float maskNext = smoothstep(0.105, 0.205, mask);
+  float mask = t * 1.08 - (slide * 0.6 + noiseR * 0.2 + noiseG * 0.2);
+  float maskPrev = 1.0 - smoothstep(0.0, 0.04, mask);
+  float maskNext = smoothstep(0.04, 0.08, mask);
 
   vec3 color1 = vec3(1.0, 0.0, 0.0) * maskPrev;
   vec3 color2 = vec3(0.0, 1.0, 0.0) * maskNext;
