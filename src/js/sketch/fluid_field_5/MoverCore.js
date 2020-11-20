bb886386b7a97ddf097e85b1ac1f92b13aa45804
yoichi kobayashi <info@tplh.net>
2020-11-21 02:47:37 +0900

    fixed the mover trail.

--- src/js/sketch/fluid_field_5/MoverCore.js
+++ src/js/sketch/fluid_field_5/MoverCore.js
@@ -7,7 +7,7 @@ export default class MoverCore extends THREE.InstancedMesh {
   constructor(count) {
     // Define Geometry
     const geometry = new THREE.InstancedBufferGeometry();
-    const baseGeometry = new THREE.PlaneBufferGeometry(4, 4);
+    const baseGeometry = new THREE.PlaneBufferGeometry(5, 5);
 
     // Add common attributes
     geometry.copy(baseGeometry);
--- src/js/sketch/fluid_field_5/MoverTrail.js
+++ src/js/sketch/fluid_field_5/MoverTrail.js
@@ -7,7 +7,8 @@ export default class MoverTrail extends THREE.InstancedMesh {
   constructor(count, heightSegments) {
     // Define Geometry
     const geometry = new THREE.InstancedBufferGeometry();
-    const baseGeometry = new THREE.CylinderBufferGeometry(0, 2, 2, 6, heightSegments, true);
+    const baseGeometry = new THREE.CylinderBufferGeometry(0, 1.5, 2, 6, heightSegments, true);
+    console.log(baseGeometry);
 
     // Add common attributes
     geometry.copy(baseGeometry);
