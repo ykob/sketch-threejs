a384f4ddb633f0d1ebf01dfc75627c9c7549159c
yoichi kobayashi <info@tplh.net>
2020-11-25 19:06:29 +0900

    updated the animation of the mover.

--- src/js/sketch/flow_field_5/glsl/physicsRendererAcceleration.fs
+++ src/js/sketch/flow_field_5/glsl/physicsRendererAcceleration.fs
@@ -18,17 +18,17 @@ void main(void) {
   vec3 a = texture2D(acceleration, vUv).xyz;
   vec3 hopt = texture2D(hookOptions, vUv).xyz;
   vec3 d = drag(a, hopt.r);
-  vec3 h = hook(v, anchor, 40.0, hopt.g);
+  vec3 h = hook(v, anchor, 50.0, hopt.g);
 
-  float texColorR = texture2D(noiseTex, v.zy * 0.0024 + step(vUv.x, 0.5) * 0.333 + step(vUv.y, 0.5) * 0.333 + time * 0.04 + multiTime).r;
-  float texColorG = texture2D(noiseTex, v.xz * 0.0024 + step(vUv.x, 0.5) * 0.333 + step(vUv.y, 0.5) * 0.333 - time * 0.04 + multiTime).g;
-  float texColorB = texture2D(noiseTex, v.yx * 0.0024 + step(vUv.x, 0.5) * 0.333 + step(vUv.y, 0.5) * 0.333 + time * 0.04 - multiTime).b;
+  float texColorR = texture2D(noiseTex, v.zy * 0.0025 + step(vUv.x, 0.333) * 0.333 + step(vUv.x, 0.666) * 0.333 + time * 0.04 + multiTime).r;
+  float texColorG = texture2D(noiseTex, v.xz * 0.0025 + step(vUv.x, 0.333) * 0.333 + step(vUv.x, 0.666) * 0.333 - time * 0.04 + multiTime).g;
+  float texColorB = texture2D(noiseTex, v.yx * 0.0025 + step(vUv.x, 0.333) * 0.333 + step(vUv.x, 0.666) * 0.333 + time * 0.04 - multiTime).b;
   vec3 noise = vec3(
     texColorR * 2.0 - 1.0,
     texColorG * 2.0 - 1.0,
     texColorB * 2.0 - 1.0
   );
-  vec3 f = noise * 0.8;
+  vec3 f = noise * 0.85;
 
   vec3 f2 = a + d + f + h;
 
