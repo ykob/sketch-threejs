0caf938ff9194e596a4762985fa6e68e90e983c7
Yoichi Kobayashi <info@tplh.net>
2021-04-03 00:23:10 +0900

    update the url of my website.

--- src/js/common/ConsoleSignature.js
+++ src/js/common/ConsoleSignature.js
@@ -1,7 +1,7 @@
 export default class ConsoleSignature {
   constructor() {
     this.message = `created by yoichi kobayashi`;
-    this.url = `http://www.tplh.net`;
+    this.url = `https://www.tplh.net/`;
     this.show();
   }
   show() {
